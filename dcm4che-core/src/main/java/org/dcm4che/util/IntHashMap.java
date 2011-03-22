e275bdd07b480f7b3a148cbba442f1f5db8e0728
Gunter Zeilinger <gunterze@gmail.com>
2011-03-22 11:52:42 +0100

    add IntHashMap<V>

---
+++ dcm4che-core/src/main/java/org/dcm4che/util/IntHashMap.java
@@ -0,0 +1,238 @@
+package org.dcm4che.util;
+
+import java.util.Arrays;
+
+public class IntHashMap<V> implements Cloneable, java.io.Serializable {
+
+    private static final int DEFAULT_CAPACITY = 32;
+    private static final int MINIMUM_CAPACITY = 4;
+    private static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    private static final byte FREE = 0;
+    private static final byte FULL = 1;
+    private static final byte REMOVED = -1;
+
+    private transient int[] keys;
+    private transient Object[] values;
+    private transient byte[] states;
+    private transient int free;
+    private transient int size;
+
+    public IntHashMap() {
+        init(DEFAULT_CAPACITY);
+    }
+
+    public IntHashMap(int expectedMaxSize) {
+        if (expectedMaxSize < 0)
+            throw new IllegalArgumentException(
+                    "expectedMaxSize is negative: " + expectedMaxSize);
+
+        init(capacity(expectedMaxSize));
+    }
+
+    private int capacity(int expectedMaxSize) {
+        int minCapacity = expectedMaxSize << 1;
+        if (minCapacity > MAXIMUM_CAPACITY)
+            return MAXIMUM_CAPACITY;
+
+        int capacity = MINIMUM_CAPACITY;
+        while (capacity < minCapacity)
+            capacity <<= 1;
+
+        return capacity;
+    }
+
+    private void init(int initCapacity) {
+        keys = new int[initCapacity];
+        values = new Object[initCapacity];
+        states = new byte[initCapacity];
+        free = initCapacity >>> 1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    public V get(int key) {
+        byte[] states = this.states;
+        int[] keys = this.keys;
+        int mask = keys.length - 1;
+        int i = key & mask;
+        while (true) {
+            if (keys[i] == key)
+                return (V) values[i];
+            if (states[i] == FREE)
+                return null;
+            i = (i + 1) & mask;
+        }
+    }
+
+    public boolean containsKey(int key) {
+        byte[] states = this.states;
+        int[] keys = this.keys;
+        int mask = keys.length - 1;
+        int i = key & mask;
+        while (true) {
+            if (keys[i] == key)
+                return true;
+            if (states[i] == FREE)
+                return false;
+            i = (i + 1) & mask;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public V put(int key, V value) {
+        byte[] states = this.states;
+        int[] keys = this.keys;
+        int mask = keys.length - 1;
+        int i = key & mask;
+
+        while (states[i] != FREE) {
+            if (keys[i] == key) {
+                V oldValue = (V) values[i];
+                values[i] = value;
+                if (states[i] < FREE) { // states[i] == REMOVED
+                    states[i] = FULL; 
+                    ++size;
+                }
+                return oldValue;
+            }
+            i = (i + 1) & mask;
+        }
+
+        states[i] = FULL;
+        keys[i] = key;
+        values[i] = value;
+        ++size;
+        if (--free <= 0)
+            resize(keys.length << 1);
+        return null;
+    }
+
+    public void rehash(boolean trimToSize) {
+        resize(trimToSize ? capacity(size) : keys.length);
+    }
+
+    private void resize(int newLength) {
+        if (newLength > MAXIMUM_CAPACITY)
+            throw new IllegalStateException("Capacity exhausted.");
+
+        int[] oldKeys = keys;
+        Object[] oldValues = values;
+        byte[] oldStates = states;
+        int[] newKeys = new int[newLength];
+        Object[] newValues = new Object[newLength];
+        byte[] newStates = new byte[newLength];
+        int mask = newLength - 1;
+
+        for (int j = 0; j < oldKeys.length; j++) {
+            if (oldStates[j] > 0) { // states[i] == FULL
+                int key = oldKeys[j];
+                int i = key & mask;
+                while (newStates[i] != FREE)
+                    i = (i + 1) & mask;
+                newStates[i] = FULL;
+                newKeys[i] = key;
+                newValues[i] = oldValues[j];
+                oldValues[j] = null;
+            }
+        }
+        keys = newKeys;
+        values = newValues;
+        states = newStates;
+        free = (newLength >>> 1) - size;
+    }
+
+    @SuppressWarnings("unchecked")
+    public V remove(int key) {
+        byte[] states = this.states;
+        int[] keys = this.keys;
+        int mask = keys.length - 1;
+        int i = key & mask;
+        while (states[i] != FREE) {
+            if (keys[i] == key) {
+                if (states[i] < FREE) // states[i] == REMOVED
+                    return null;
+
+                states[i] = REMOVED;
+                V oldValue = (V) values[i];
+                values[i] = null;
+                size--;
+                return oldValue;
+            }
+            i = (i + 1) & mask;
+        }
+        return null;
+    }
+
+    public void clear() {
+        Arrays.fill(values, null);
+        Arrays.fill(states, FREE);
+        size = 0;
+        free = keys.length >>> 1;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Object clone() {
+        try {
+            IntHashMap<V> m = (IntHashMap<V>) super.clone();
+            m.states = states;
+            m.keys = keys;
+            m.values = values;
+            m.free = free;
+            return m;
+        } catch (CloneNotSupportedException e) {
+            throw new InternalError();
+        }
+    }
+
+    private static final long serialVersionUID = 9153226350279204066L;
+
+    private void writeObject(java.io.ObjectOutputStream s)
+            throws java.io.IOException {
+        s.defaultWriteObject();
+
+        byte[] states = this.states;
+        int[] keys = this.keys;
+        Object[] values = this.values;
+        s.writeInt(size);
+        for (int i = 0; i < states.length; i++) {
+            if (states[i] > FREE) { // states[i] == FULL
+                s.writeInt(keys[i]);
+                s.writeObject(values[i]);
+            }
+        }
+    }
+
+    private void readObject(java.io.ObjectInputStream s)
+            throws java.io.IOException, ClassNotFoundException  {
+        s.defaultReadObject();
+
+        int count = s.readInt();
+        init(capacity(count));
+        size = count;
+        free -= count;
+
+        byte[] states = this.states;
+        int[] keys = this.keys;
+        Object[] values = this.values;
+        int mask = keys.length - 1;
+
+        while (count-- > 0) {
+            int key = s.readInt();
+            int i = key & mask;
+            while (states[i] != FREE)
+                i = (i + 1) & mask;
+            states[i] = FULL;
+            keys[i] = key;
+            values[i] = s.readObject();
+        }
+    }
+}
---
+++ dcm4che-core/src/test/java/org/dcm4che/util/IntHashMapTest.java
@@ -0,0 +1,118 @@
+package org.dcm4che.util;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class IntHashMapTest {
+
+    private IntHashMap<Integer> map;
+
+    @Before
+    public void setUp() throws Exception {
+        map = new IntHashMap<Integer>();
+        for (int i = 1; i < 45; i += 3)
+            map.put(i, Integer.valueOf(i));
+    }
+
+    private void removeOdd() {
+        for (int i = 1; i < 45; i += 2)
+            map.remove(i);
+    }
+
+    private static void testGet(IntHashMap<Integer> map) {
+        for (int i = 1; i < 45; i++)
+            if ((i & 1) == 0 && (i % 3) == 1)
+                assertEquals(Integer.valueOf(i), map.get(i));
+            else
+                assertNull(map.get(i));
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(15, map.size());
+        removeOdd();
+        assertEquals(7, map.size());
+    }
+
+    @Test
+    public void testGet() {
+        removeOdd();
+        testGet(map);
+    }
+
+    @Test
+    public void testPut() {
+        removeOdd();
+        for (int i = 0; i < 45; i++)
+            map.put(i, Integer.valueOf(i));
+        assertEquals(45, map.size());
+        for (int i = 0; i < 45; i++)
+            assertEquals(Integer.valueOf(i), map.get(i));
+    }
+
+    @Test
+    public void testContainsKey() {
+        for (int i = 1; i < 45; i++)
+            assertEquals((i % 3) == 1, map.containsKey(i));
+    }
+
+    @Test
+    public void testRehash() {
+        removeOdd();
+        map.rehash(true);
+        testGet(map);
+   }
+
+    @Test
+    public void testRemove() {
+        for (int i = 1; i < 45; i += 2)
+            if ((i % 3) == 1)
+                assertEquals(Integer.valueOf(i), map.remove(i));
+            else
+                assertNull(map.remove(i));
+    }
+
+    @Test
+    public void testClear() {
+        assertFalse(map.isEmpty());
+        map.clear();
+        assertTrue(map.isEmpty());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testClone() {
+        removeOdd();
+        testGet((IntHashMap<Integer>) map.clone());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testSerialize() throws Exception {
+        removeOdd();
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        ObjectOutputStream oout = new ObjectOutputStream(bout);
+        try {
+            oout.writeObject(map);
+            oout.writeUTF("EOF");
+        } finally {
+            oout.close();
+        }
+        ByteArrayInputStream bin = new ByteArrayInputStream(
+                bout.toByteArray());
+        ObjectInputStream oin = new ObjectInputStream(bin);
+        try {
+            testGet((IntHashMap<Integer>) oin.readObject());
+            assertEquals("EOF", oin.readUTF());
+        } finally {
+            oin.close();
+        }
+    }
+}

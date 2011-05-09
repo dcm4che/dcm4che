4fa50f94d8f1c07bdee80b769b9ed9c3bd05bd51
Gunter Zeilinger <gunterze@gmail.com>
2011-05-09 15:34:28 +0200

    implementing findscu

---
+++ dcm4che-tool/dcm4che-tool-findscu/src/bin/findscu.bat
@@ -0,0 +1,53 @@
+@echo off
+rem -------------------------------------------------------------------------
+rem findscu  Launcher
+rem -------------------------------------------------------------------------
+
+if not "%ECHO%" == ""  echo %ECHO%
+if "%OS%" == "Windows_NT"  setlocal
+
+set MAIN_CLASS=org.dcm4che2.tool.findscu.Main
+set MAIN_JAR=dcm4che-tool-findscu-3.0.0-SNAPSHOT.jar
+
+set DIRNAME=.\
+if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%
+
+rem Read all command line arguments
+
+set ARGS=
+:loop
+if [%1] == [] goto end
+        set ARGS=%ARGS% %1
+        shift
+        goto loop
+:end
+
+if not "%DCM4CHE_HOME%" == "" goto HAVE_DCM4CHE_HOME
+
+set DCM4CHE_HOME=%DIRNAME%..
+
+:HAVE_DCM4CHE_HOME
+
+if not "%JAVA_HOME%" == "" goto HAVE_JAVA_HOME
+
+set JAVA=java
+
+goto SKIP_SET_JAVA_HOME
+
+:HAVE_JAVA_HOME
+
+set JAVA=%JAVA_HOME%\bin\java
+
+:SKIP_SET_JAVA_HOME
+
+set CP=%DCM4CHE_HOME%\etc\findscu\
+set CP=%CP%;%DCM4CHE_HOME%\lib\%MAIN_JAR%
+set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-core-3.0.0-SNAPSHOT.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-net-3.0.0-SNAPSHOT.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-tool-common-3.0.0-SNAPSHOT.jar"
+set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-api-1.6.1.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-log4j12-1.6.1.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\log4j-1.2.16.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\commons-cli-1.2.jar
+
+"%JAVA%" %JAVA_OPTS% -cp "%CP%" %MAIN_CLASS% %ARGS%

7c62532f52194c87317b3dbf00225fead513abfc
Gunter Zeilinger <gunterze@gmail.com>
2011-07-20 14:48:38 +0200

    Fix windows launcher scripts

--- dcm4che-tool/dcm4che-tool-dcm2xml/src/bin/dcm2xml.bat
+++ dcm4che-tool/dcm4che-tool-dcm2xml/src/bin/dcm2xml.bat
@@ -6,7 +6,7 @@ rem -------------------------------------------------------------------------
 if not "%ECHO%" == ""  echo %ECHO%
 if "%OS%" == "Windows_NT"  setlocal
 
-set MAIN_CLASS=org.dcm4che2.tool.dcm2xml.Main
+set MAIN_CLASS=org.dcm4che.tool.dcm2xml.Main
 set MAIN_JAR=dcm4che-tool-dcm2xml-3.0.0-SNAPSHOT.jar
 
 set DIRNAME=.\
--- dcm4che-tool/dcm4che-tool-dcmqrscp/src/bin/dcmqrscp.bat
+++ dcm4che-tool/dcm4che-tool-dcmqrscp/src/bin/dcmqrscp.bat
@@ -6,7 +6,7 @@ rem -------------------------------------------------------------------------
 if not "%ECHO%" == ""  echo %ECHO%
 if "%OS%" == "Windows_NT"  setlocal
 
-set MAIN_CLASS=org.dcm4che2.tool.dcmqrscp.Main
+set MAIN_CLASS=org.dcm4che.tool.dcmqrscp.Main
 set MAIN_JAR=dcm4che-tool-dcmqrscp-3.0.0-SNAPSHOT.jar
 
 set DIRNAME=.\

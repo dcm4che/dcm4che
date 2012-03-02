0f28d3818b78eb893f975f3709de44584d89deb4
Gunter Zeilinger <gunterze@gmail.com>
2012-03-02 14:46:22 +0100

    add HL7Utils.toDicomCharacterSetCode(String)

--- dcm4che-hl7/src/main/java/org/dcm4che/hl7/HL7Utils.java
+++ dcm4che-hl7/src/main/java/org/dcm4che/hl7/HL7Utils.java
@@ -125,9 +125,65 @@ public abstract class HL7Utils {
         return end < 0 ? msgtype : msgtype.substring(0, end);
     }
 
-    public static String charsetName(String[] msh, String defCode) {
-        String code = msh.length > 17 ? msh[17] : null;
-        return charsetName(code != null && !code.isEmpty() ? code : defCode);
+    public static String toDicomCharacterSetCode(String code) {
+        if (code != null && !code.isEmpty())
+            switch (code.charAt(code.length()-1)) {
+            case '0':
+                if (code.equals("GB 18030-2000"))
+                    return "GB18030";
+                break;
+            case '1':
+                if (code.equals("8859/1"))
+                    return "ISO_IR 100";
+                if (code.equals("KS X 1001"))
+                    return "EUC-KR";
+                break;
+            case '2':
+                if (code.equals("8859/2"))
+                    return "ISO_IR 101";
+                break;
+            case '3':
+                if (code.equals("8859/3"))
+                    return "ISO_IR 109";
+                break;
+            case '4':
+                if (code.equals("8859/4"))
+                    return "ISO_IR 110";
+                if (code.equals("ISO IR14"))
+                    return "JIS_X0201";
+                break;
+            case '5':
+                if (code.equals("8859/5"))
+                    return "ISO_IR 144";
+                break;
+            case '6':
+                if (code.equals("8859/6"))
+                    return "ISO_IR 127";
+                break;
+            case '7':
+                if (code.equals("8859/7"))
+                    return "ISO_IR 126";
+                if (code.equals("ISO IR87"))
+                    return "ISO 2022 IR 87";
+                break;
+            case '8':
+                if (code.equals("8859/8"))
+                    return "ISO_IR 138";
+                if (code.equals("UNICODE UTF-8"))
+                    return "ISO_IR 192";
+                break;
+            case '9':
+                if (code.equals("8859/9"))
+                    return "ISO_IR 148";
+                if (code.equals("ISO IR159"))
+                    return "ISO 2022 IR 159";
+                break;
+            case 'E':
+                if (code.equals("UNICODE"))
+                    return "ISO_IR 192";
+                break;
+            }
+        return null;
     }
 
     public static String charsetName(String code) {

7555d40fbf88afcbee9d82142785c207b7e3ce31
gunterze <gunterze@gmail.com>
2012-11-23 09:01:01 +0100

    increment version to 3.0.1

--- dcm4che-soundex/src/bin/phonem.bat
+++ dcm4che-soundex/src/bin/phonem.bat
@@ -7,7 +7,7 @@ if not "%ECHO%" == ""  echo %ECHO%
 if "%OS%" == "Windows_NT"  setlocal
 
 set MAIN_CLASS=org.dcm4che.soundex.Phonem
-set MAIN_JAR=dcm4che-soundex-3.0.0.jar
+set MAIN_JAR=dcm4che-soundex-3.0.1.jar
 
 set DIRNAME=.\
 if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%

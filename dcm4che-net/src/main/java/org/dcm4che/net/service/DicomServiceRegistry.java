b4ccf76a773a43bb25c7e76c7a2663ba8f604cf1
gunterze <gunterze@gmail.com>
2013-10-18 12:52:18 +0200

    [#LIB-129] DicomServiceFactory: log DicomServiceException in onDimseRQ() with INFO level

--- dcm4che-net/src/main/java/org/dcm4che/net/service/DicomServiceRegistry.java
+++ dcm4che-net/src/main/java/org/dcm4che/net/service/DicomServiceRegistry.java
@@ -77,7 +77,7 @@ public class DicomServiceRegistry implements DimseRQHandler {
         try {
             lookupService(as, dimse, cmd).onDimseRQ(as, pc, dimse, cmd, data);
         } catch (DicomServiceException e) {
-            Association.LOG.info("{}: processing {} fails:",
+            Association.LOG.info("{}: processing {} failed:",
                     as,
                     dimse.toString(cmd, pc.getPCID(), pc.getTransferSyntax()),
                     e);

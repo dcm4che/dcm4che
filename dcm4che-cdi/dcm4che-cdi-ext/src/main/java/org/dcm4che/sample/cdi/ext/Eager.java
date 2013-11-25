5efeae5c791aa762d7bab2927816df44a346f58e
gunterze <gunterze@gmail.com>
2013-11-25 16:04:05 +0100

    [#LIB-137] sample echo SCP as CDI service

---
+++ dcm4che-cdi/dcm4che-cdi-ext/src/main/java/org/dcm4che/sample/cdi/ext/Eager.java
@@ -0,0 +1,12 @@
+package org.dcm4che.sample.cdi.ext;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Eager {
+
+}

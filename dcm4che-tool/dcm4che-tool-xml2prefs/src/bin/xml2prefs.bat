13ed240c219a3a5872456fe9af91c14d963029eb
gunterze <gunterze@gmail.com>
2014-03-06 12:07:08 +0100

    [#LIB-154] release dcm4che-3.3.0

--- dcm4che-tool/dcm4che-tool-xml2prefs/src/bin/xml2prefs.bat
+++ dcm4che-tool/dcm4che-tool-xml2prefs/src/bin/xml2prefs.bat
@@ -7,7 +7,7 @@ if not "%ECHO%" == ""  echo %ECHO%
 if "%OS%" == "Windows_NT"  setlocal
 
 set MAIN_CLASS=org.dcm4che3.tool.xml2prefs.Xml2Prefs
-set MAIN_JAR=dcm4che-tool-xml2prefs-3.3.0.jar
+set MAIN_JAR=dcm4che-tool-xml2prefs-3.3.1-SNAPSHOT.jar
 
 set DIRNAME=.\
 if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%

4f6b24905c977a2b5124f04e96566927d028f5a4
Hesham Elbadawi <bsdreko@gmail.com>
2014-05-30 12:07:33 +0200

    [#LIB-206  Migrate Jpg2Dcm tool from dcm4che2 ]
    Note:
    in case of mpeg and mp4 files the mpeg2dcm.xml file is used to fill
    attributes, possible differences between supplied attributes (match the
    sample.mpeg in the etc dir) and the new file attributes should be
    manually changed by the user.

---
+++ dcm4che-tool/dcm4che-tool-jpg2dcm/src/bin/jpg2dcm.bat
@@ -0,0 +1,55 @@
+@echo off
+rem -------------------------------------------------------------------------
+rem dcm4che2/jpg2dcm  Launcher
+rem -------------------------------------------------------------------------
+
+rem $Id: jpg2dcm.bat 18015 2013-08-27 12:03:33Z gunterze $
+
+if not "%ECHO%" == ""  echo %ECHO%
+if "%OS%" == "Windows_NT"  setlocal
+
+set MAIN_CLASS=org.dcm4che3.tool.jpg2dcm.Jpg2Dcm
+set MAIN_JAR=dcm4che-tool-jpg2dcm-3.3.4-SNAPSHOT.jar
+
+set DIRNAME=.\
+if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%
+
+rem Read all command line arguments
+
+set ARGS=
+:loop
+if [%1] == [] goto end
+        set ARGS=%ARGS% %1
+        shift
+        goto loop
+:end
+
+if not "%DCM4CHE_HOME%" == "" goto HAVE_DCM4CHE_HOME
+
+set DCM4CHE_HOME=%DIRNAME%..
+
+:HAVE_DCM4CHE_HOME
+
+if not "%JAVA_HOME%" == "" goto HAVE_JAVA_HOME
+
+set JAVA=java
+
+goto SKIP_SET_JAVA_HOME
+
+:HAVE_JAVA_HOME
+
+set JAVA=%JAVA_HOME%\bin\java
+
+:SKIP_SET_JAVA_HOME
+
+set CP=%DCM4CHE_HOME%\etc\
+set CP=%CP%;%DCM4CHE_HOME%\lib\%MAIN_JAR%
+set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-core-3.3.4-SNAPSHOT.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-tool-common-3.3.4-SNAPSHOT.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-api-1.7.5.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-log4j12-1.7.5.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\log4j-1.2.17.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\commons-cli-1.2.jar
+
+"%JAVA%" %JAVA_OPTS% -cp "%CP%" %MAIN_CLASS% %ARGS%
+

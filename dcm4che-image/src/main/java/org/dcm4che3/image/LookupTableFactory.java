4e23785ca490e5bf2dcf735d7dd4c16643bd6b9d
Padraig-iMac <padraig.looney@nhs.net>
2014-09-10 10:17:25 +0100

    Allowing the setting of the window centre and width from the Shared Functional Group

--- dcm4che-image/src/main/java/org/dcm4che3/image/LookupTableFactory.java
+++ dcm4che-image/src/main/java/org/dcm4che3/image/LookupTableFactory.java
@@ -46,6 +46,7 @@ import java.awt.image.DataBufferUShort;
 import java.awt.image.Raster;
 
 import org.dcm4che3.data.Attributes;
+import org.dcm4che3.data.Sequence;
 import org.dcm4che3.data.Tag;
 import org.dcm4che3.util.ByteUtils;
 
@@ -55,329 +56,347 @@ import org.dcm4che3.util.ByteUtils;
  */
 public class LookupTableFactory {
 
-	private final StoredValue storedValue;
-	private float rescaleSlope = 1;
-	private float rescaleIntercept = 0;
-	private LookupTable modalityLUT;
-	private float windowCenter;
-	private float windowWidth;
-	private String voiLUTFunction; // not yet implemented
-	private LookupTable voiLUT;
-	private LookupTable presentationLUT;
-	private boolean inverse;
+    private final StoredValue storedValue;
+    private float rescaleSlope = 1;
+    private float rescaleIntercept = 0;
+    private LookupTable modalityLUT;
+    private float windowCenter;
+    private float windowWidth;
+    private String voiLUTFunction; // not yet implemented
+    private LookupTable voiLUT;
+    private LookupTable presentationLUT;
+    private boolean inverse;
 
-	public LookupTableFactory(StoredValue storedValue) {
-		this.storedValue = storedValue;
-	}
+    public LookupTableFactory(StoredValue storedValue) {
+	this.storedValue = storedValue;
+    }
 
-	public void setModalityLUT(Attributes attrs) {
-		rescaleIntercept = attrs.getFloat(Tag.RescaleIntercept, 0);
-		rescaleSlope = attrs.getFloat(Tag.RescaleSlope, 1);
-		modalityLUT = createLUT(storedValue,
-				attrs.getNestedDataset(Tag.ModalityLUTSequence));
+    public void setModalityLUT(Attributes attrs) {
+	rescaleIntercept = attrs.getFloat(Tag.RescaleIntercept, 0);
+	rescaleSlope = attrs.getFloat(Tag.RescaleSlope, 1);
+	modalityLUT = createLUT(storedValue,
+		attrs.getNestedDataset(Tag.ModalityLUTSequence));
 
-	}
+    }
 
-	public void setPresentationLUT(Attributes attrs) {
-		Attributes pLUT = attrs.getNestedDataset(Tag.PresentationLUTSequence);
-		if (pLUT != null) {
-			int[] desc = pLUT.getInts(Tag.LUTDescriptor);
-			if (desc != null && desc.length == 3) {
-				int len = desc[0] == 0 ? 0x10000 : desc[0];
-				presentationLUT = createLUT(new StoredValue.Unsigned(log2(len)), 
-						resetOffset(desc), 
-						pLUT.getSafeBytes(Tag.LUTData), pLUT.bigEndian());
-			}
-		} else {
-			String pShape = attrs.getString(Tag.PresentationLUTShape);
-			inverse = (pShape != null 
-					? "INVERSE".equals(pShape)
-							: "MONOCHROME1".equals(
-									attrs.getString(Tag.PhotometricInterpretation)));
-		}
-	}
-
-	private int[] resetOffset(int[] desc) {
-		if (desc[1] == 0)
-			return desc;
-
-		int[] copy = desc.clone();
-		copy[1] = 0;
-		return copy;
-	}
-
-	public void setWindowCenter(float windowCenter) {
-		this.windowCenter = windowCenter;
-	}
-
-	public void setWindowWidth(float windowWidth) {
-		this.windowWidth = windowWidth;
-	}
-
-	public void setVOI(Attributes img, int windowIndex, int voiLUTIndex,
-			boolean preferWindow) {
-		if (img == null)
-			return;
-
-		Attributes vLUT = img.getNestedDataset(Tag.VOILUTSequence, voiLUTIndex);
-		if (preferWindow || vLUT == null) {
-			float[] wcs = img.getFloats(Tag.WindowCenter);
-			float[] wws = img.getFloats(Tag.WindowWidth);
-			if (wcs != null && wcs.length != 0
-					&& wws != null && wws.length != 0) {
-				int index = windowIndex < Math.min(wcs.length, wws.length)
-						? windowIndex
-								: 0;
-				windowCenter = wcs[index];
-				windowWidth = wws[index];
-
-				//return;
-			}
-		}
-		voiLUTFunction = img.getString(Tag.VOILUTFunction);
-		if (vLUT != null)
-			voiLUT = createLUT(modalityLUT != null
-			? new StoredValue.Unsigned(modalityLUT.outBits)
-			: storedValue,
-			vLUT);
-		else if(voiLUTFunction != null && voiLUTFunction.equals("SIGMOID")) {
-
-			StoredValue inBits = modalityLUT != null
-					? new StoredValue.Unsigned(modalityLUT.outBits)
-			: storedValue;
-					voiLUT = createSigmoidLUT(inBits, 8);
-		}
-
-		return;
-
-	}
-
-	private LookupTable createLUT(StoredValue inBits, Attributes attrs) {
-		if (attrs == null)
-			return null;
-
-		return createLUT(inBits, attrs.getInts(Tag.LUTDescriptor),
-				attrs.getSafeBytes(Tag.LUTData), attrs.bigEndian());
-	}
-
-	private LookupTable createLUT(StoredValue inBits, int[] desc, byte[] data,
-			boolean bigEndian) {
-
-		if (desc == null)
-			return null;
-
-		if (desc.length != 3)
-			return null;
-
+    public void setPresentationLUT(Attributes attrs) {
+	Attributes pLUT = attrs.getNestedDataset(Tag.PresentationLUTSequence);
+	if (pLUT != null) {
+	    int[] desc = pLUT.getInts(Tag.LUTDescriptor);
+	    if (desc != null && desc.length == 3) {
 		int len = desc[0] == 0 ? 0x10000 : desc[0];
-		int offset = (short) desc[1];
-		int outBits = desc[2];
-		if (data == null)
-			return null;
+		presentationLUT = createLUT(new StoredValue.Unsigned(log2(len)), 
+			resetOffset(desc), 
+			pLUT.getSafeBytes(Tag.LUTData), pLUT.bigEndian());
+	    }
+	} else {
+	    String pShape = attrs.getString(Tag.PresentationLUTShape);
+	    inverse = (pShape != null 
+		    ? "INVERSE".equals(pShape)
+			    : "MONOCHROME1".equals(
+				    attrs.getString(Tag.PhotometricInterpretation)));
+	}
+    }
 
-		if (data.length == len << 1) {
-			if (outBits > 8) {
-				if (outBits > 16)
-					return null;
+    private int[] resetOffset(int[] desc) {
+	if (desc[1] == 0)
+	    return desc;
 
-				short[] ss = new short[len];
-				if (bigEndian)
-					for (int i = 0; i < ss.length; i++)
-						ss[i] = (short) ByteUtils.bytesToShortBE(data, i << 1);
-				else
-					for (int i = 0; i < ss.length; i++)
-						ss[i] = (short) ByteUtils.bytesToShortLE(data, i << 1);
+	int[] copy = desc.clone();
+	copy[1] = 0;
+	return copy;
+    }
 
-				return new ShortLookupTable(inBits, outBits, offset, ss);
-			}
-			// padded high bits -> use low bits
-			data = halfLength(data, bigEndian ? 1 : 0);
+    public void setWindowCenter(float windowCenter) {
+	this.windowCenter = windowCenter;
+    }
+
+    public void setWindowWidth(float windowWidth) {
+	this.windowWidth = windowWidth;
+    }
+
+    private float[] getSharedFunctional(Attributes metadata){
+	Sequence ds = metadata.getSequence(Tag.SharedFunctionalGroupsSequence);
+	if(ds == null) return null;
+	Attributes attribs = ds.get(0);
+	ds = attribs.getSequence(Tag.FrameVOILUTSequence);
+	attribs = ds.get(0);
+	float f1 = attribs.getFloat(Tag.WindowCenter, 0);
+	float f2 = attribs.getFloat(Tag.WindowWidth, 0);
+	float[] fs = {f1,f2};
+	return fs;
+    }
+
+    public void setVOI(Attributes img, int windowIndex, int voiLUTIndex,
+	    boolean preferWindow) {
+	if (img == null)
+	    return;
+
+	Attributes vLUT = img.getNestedDataset(Tag.VOILUTSequence, voiLUTIndex);
+	if (preferWindow || vLUT == null) {
+	    float[] wcs = img.getFloats(Tag.WindowCenter);
+	    float[] wws = img.getFloats(Tag.WindowWidth);
+	    if (wcs != null && wcs.length != 0
+		    && wws != null && wws.length != 0) {
+		int index = windowIndex < Math.min(wcs.length, wws.length)
+			? windowIndex
+				: 0;
+		windowCenter = wcs[index];
+		windowWidth = wws[index];
+
+		//return;
+	    } else{
+		float[] fs = this.getSharedFunctional(img);
+		if(fs!=null){
+		    windowCenter = fs[0];
+		    windowWidth = fs[1];
 		}
-		if (data.length != len)
-			return null;
+	    }
+	}
+	voiLUTFunction = img.getString(Tag.VOILUTFunction);
+	if (vLUT != null)
+	    voiLUT = createLUT(modalityLUT != null
+	    ? new StoredValue.Unsigned(modalityLUT.outBits)
+	    : storedValue,
+	    vLUT);
+	else if(voiLUTFunction != null && voiLUTFunction.equals("SIGMOID")) {
 
-		if (outBits > 8)
-			return null;
-
-		return new ByteLookupTable(inBits, outBits, offset, data);
+	    StoredValue inBits = modalityLUT != null
+		    ? new StoredValue.Unsigned(modalityLUT.outBits)
+	    : storedValue;
+		    voiLUT = createSigmoidLUT(inBits, 8);
 	}
 
-	static byte[] halfLength(byte[] data, int hilo) {
-		byte[] bs = new byte[data.length >> 1];
-		for (int i = 0; i < bs.length; i++)
-			bs[i] = data[(i<<1)|hilo];
+	return;
 
-		return bs;
+    }
+
+    private LookupTable createLUT(StoredValue inBits, Attributes attrs) {
+	if (attrs == null)
+	    return null;
+
+	return createLUT(inBits, attrs.getInts(Tag.LUTDescriptor),
+		attrs.getSafeBytes(Tag.LUTData), attrs.bigEndian());
+    }
+
+    private LookupTable createLUT(StoredValue inBits, int[] desc, byte[] data,
+	    boolean bigEndian) {
+
+	if (desc == null)
+	    return null;
+
+	if (desc.length != 3)
+	    return null;
+
+	int len = desc[0] == 0 ? 0x10000 : desc[0];
+	int offset = (short) desc[1];
+	int outBits = desc[2];
+	if (data == null)
+	    return null;
+
+	if (data.length == len << 1) {
+	    if (outBits > 8) {
+		if (outBits > 16)
+		    return null;
+
+		short[] ss = new short[len];
+		if (bigEndian)
+		    for (int i = 0; i < ss.length; i++)
+			ss[i] = (short) ByteUtils.bytesToShortBE(data, i << 1);
+		else
+		    for (int i = 0; i < ss.length; i++)
+			ss[i] = (short) ByteUtils.bytesToShortLE(data, i << 1);
+
+		return new ShortLookupTable(inBits, outBits, offset, ss);
+	    }
+	    // padded high bits -> use low bits
+	    data = halfLength(data, bigEndian ? 1 : 0);
+	}
+	if (data.length != len)
+	    return null;
+
+	if (outBits > 8)
+	    return null;
+
+	return new ByteLookupTable(inBits, outBits, offset, data);
+    }
+
+    static byte[] halfLength(byte[] data, int hilo) {
+	byte[] bs = new byte[data.length >> 1];
+	for (int i = 0; i < bs.length; i++)
+	    bs[i] = data[(i<<1)|hilo];
+
+	return bs;
+    }
+
+    public LookupTable createLUT(int outBits) {
+	LookupTable lut = combineModalityVOILUT(presentationLUT != null
+		? log2(presentationLUT.length())
+			: outBits);
+	if (presentationLUT != null) {
+	    lut = lut.combine(presentationLUT.adjustOutBits(outBits));
+	} else if (inverse)
+	    lut.inverse();
+	return lut;
+    }
+
+    private static int log2(int value) {
+	int i = 0;
+	while ((value>>>i) != 0)
+	    ++i;
+	return i-1;
+    }
+
+    private LookupTable combineModalityVOILUT(int outBits) {
+	float m = rescaleSlope;
+	float b = rescaleIntercept;
+	LookupTable modalityLUT = this.modalityLUT;
+	LookupTable lut = this.voiLUT;
+	if (lut == null) {
+	    float c = windowCenter;
+	    float w = windowWidth;
+
+	    if (w == 0 && modalityLUT != null)
+		return modalityLUT.adjustOutBits(outBits);
+
+	    int size, offset;
+	    StoredValue inBits = modalityLUT != null
+		    ? new StoredValue.Unsigned(modalityLUT.outBits)
+	    : storedValue;
+		    if (w != 0) {
+			size = Math.max(2,Math.abs(Math.round(w/m)));
+			offset = Math.round(c/m-b) - size/2;
+		    } else {
+			offset = inBits.minValue();
+			size = inBits.maxValue() - inBits.minValue() + 1;
+		    }
+		    lut = outBits > 8
+			    ? new ShortLookupTable(inBits, outBits, offset, size, m < 0)
+		    : new ByteLookupTable(inBits, outBits, offset, size, m < 0);
+	} else {
+	    //TODO consider m+b
+	    lut = lut.adjustOutBits(outBits);
+	}
+	return modalityLUT != null ? modalityLUT.combine(lut) : lut;
+    }
+
+    public boolean autoWindowing(Attributes img, Raster raster) {
+	if (modalityLUT != null || voiLUT != null || windowWidth != 0)
+	    return false;
+
+	int min = img.getInt(Tag.SmallestImagePixelValue, 0);
+	int max = img.getInt(Tag.LargestImagePixelValue, 0);
+	if (max == 0) {
+	    int[] min_max;
+	    ComponentSampleModel sm = (ComponentSampleModel) raster.getSampleModel();
+	    DataBuffer dataBuffer = raster.getDataBuffer();
+	    switch (dataBuffer.getDataType()) {
+	    case DataBuffer.TYPE_BYTE:
+		min_max = calcMinMax(storedValue, sm,
+			((DataBufferByte) dataBuffer).getData());
+		break;
+	    case DataBuffer.TYPE_USHORT:
+		min_max = calcMinMax(storedValue, sm,
+			((DataBufferUShort) dataBuffer).getData());
+		break;
+	    case DataBuffer.TYPE_SHORT:
+		min_max = calcMinMax(storedValue, sm,
+			((DataBufferShort) dataBuffer).getData());
+		break;
+	    default:
+		throw new UnsupportedOperationException(
+			"DataBuffer: "+ dataBuffer.getClass() + " not supported");
+	    }
+	    min = min_max[0];
+	    max = min_max[1];
+	}
+	windowCenter = (min + max + 1) / 2 * rescaleSlope + rescaleIntercept;
+	windowWidth = Math.abs((max + 1 - min) * rescaleSlope);
+	return true;
+    }
+
+    private int[] calcMinMax(StoredValue storedValue, ComponentSampleModel sm,
+	    byte[] data) {
+	int min = Integer.MAX_VALUE;
+	int max = Integer.MIN_VALUE;
+	int w = sm.getWidth();
+	int h = sm.getHeight();
+	int stride = sm.getScanlineStride();
+	for (int y = 0; y < h; y++)
+	    for (int i = y * stride, end = i + w; i < end;) {
+		int val = storedValue.valueOf(data[i++]);
+		if (val < min) min = val;
+		if (val > max) max = val;
+	    }
+	return new int[] { min, max };
+    }
+
+    private int[] calcMinMax(StoredValue storedValue, ComponentSampleModel sm,
+	    short[] data) {
+	int min = Integer.MAX_VALUE;
+	int max = Integer.MIN_VALUE;
+	int w = sm.getWidth();
+	int h = sm.getHeight();
+	int stride = sm.getScanlineStride();
+	for (int y = 0; y < h; y++)
+	    for (int i = y * stride, end = i + w; i < end;) {
+		int val = storedValue.valueOf(data[i++]);
+		if (val < min) min = val;
+		if (val > max) max = val;
+	    }
+	return new int[] { min, max };
+    }
+
+    public LookupTable createSigmoidLUT(StoredValue inBits,
+	    int outBits){
+
+	int outRange = 1 << outBits;
+
+	float c = windowCenter;
+	float w = windowWidth;
+	float b = rescaleIntercept;
+	float m = rescaleSlope;
+	float k = -4 * m / w;
+	int size = inBits.maxValue(); //LUT
+	int offset = 0; //Need to use no offset, not necessary for linear LUT
+
+	float ic = c;
+
+	ic = (c - b) / m -offset;
+
+	if(outBits > 8){
+	    short[] lut = new short[size];
+	    for (int i = 0; i < size; i++){
+
+		int tmp = calcSigmoid(outRange, i, ic, k);
+
+		lut[i] = (short) (tmp);
+	    }
+
+	    return new ShortLookupTable(inBits, outBits, 0, lut);
+	} else {
+	    byte[] lut = new byte[size];
+
+	    for (int i = 0; i < size; i++){
+
+		int tmp = calcSigmoid(outRange, i, ic, k);
+
+		lut[i] = (byte) ((tmp) & 0xff);
+
+	    }
+
+	    return new ByteLookupTable(inBits, outBits, 0, lut);
+
 	}
 
-	public LookupTable createLUT(int outBits) {
-		LookupTable lut = combineModalityVOILUT(presentationLUT != null
-				? log2(presentationLUT.length())
-						: outBits);
-		if (presentationLUT != null) {
-			lut = lut.combine(presentationLUT.adjustOutBits(outBits));
-		} else if (inverse)
-			lut.inverse();
-		return lut;
-	}
+    }
 
-	private static int log2(int value) {
-		int i = 0;
-		while ((value>>>i) != 0)
-			++i;
-		return i-1;
-	}
+    private int calcSigmoid(int outRange, float i, float ic, float k){
+	double num = (1 + Math.exp((i - ic) * k));
 
-	private LookupTable combineModalityVOILUT(int outBits) {
-		float m = rescaleSlope;
-		float b = rescaleIntercept;
-		LookupTable modalityLUT = this.modalityLUT;
-		LookupTable lut = this.voiLUT;
-		if (lut == null) {
-			float c = windowCenter;
-			float w = windowWidth;
-
-			if (w == 0 && modalityLUT != null)
-				return modalityLUT.adjustOutBits(outBits);
-
-			int size, offset;
-			StoredValue inBits = modalityLUT != null
-					? new StoredValue.Unsigned(modalityLUT.outBits)
-			: storedValue;
-					if (w != 0) {
-						size = Math.max(2,Math.abs(Math.round(w/m)));
-						offset = Math.round(c/m-b) - size/2;
-					} else {
-						offset = inBits.minValue();
-						size = inBits.maxValue() - inBits.minValue() + 1;
-					}
-					lut = outBits > 8
-							? new ShortLookupTable(inBits, outBits, offset, size, m < 0)
-					: new ByteLookupTable(inBits, outBits, offset, size, m < 0);
-		} else {
-			//TODO consider m+b
-			lut = lut.adjustOutBits(outBits);
-		}
-		return modalityLUT != null ? modalityLUT.combine(lut) : lut;
-	}
-
-	public boolean autoWindowing(Attributes img, Raster raster) {
-		if (modalityLUT != null || voiLUT != null || windowWidth != 0)
-			return false;
-
-		int min = img.getInt(Tag.SmallestImagePixelValue, 0);
-		int max = img.getInt(Tag.LargestImagePixelValue, 0);
-		if (max == 0) {
-			int[] min_max;
-			ComponentSampleModel sm = (ComponentSampleModel) raster.getSampleModel();
-			DataBuffer dataBuffer = raster.getDataBuffer();
-			switch (dataBuffer.getDataType()) {
-			case DataBuffer.TYPE_BYTE:
-				min_max = calcMinMax(storedValue, sm,
-						((DataBufferByte) dataBuffer).getData());
-				break;
-			case DataBuffer.TYPE_USHORT:
-				min_max = calcMinMax(storedValue, sm,
-						((DataBufferUShort) dataBuffer).getData());
-				break;
-			case DataBuffer.TYPE_SHORT:
-				min_max = calcMinMax(storedValue, sm,
-						((DataBufferShort) dataBuffer).getData());
-				break;
-			default:
-				throw new UnsupportedOperationException(
-						"DataBuffer: "+ dataBuffer.getClass() + " not supported");
-			}
-			min = min_max[0];
-			max = min_max[1];
-		}
-		windowCenter = (min + max + 1) / 2 * rescaleSlope + rescaleIntercept;
-		windowWidth = Math.abs((max + 1 - min) * rescaleSlope);
-		return true;
-	}
-
-	private int[] calcMinMax(StoredValue storedValue, ComponentSampleModel sm,
-			byte[] data) {
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		int w = sm.getWidth();
-		int h = sm.getHeight();
-		int stride = sm.getScanlineStride();
-		for (int y = 0; y < h; y++)
-			for (int i = y * stride, end = i + w; i < end;) {
-				int val = storedValue.valueOf(data[i++]);
-				if (val < min) min = val;
-				if (val > max) max = val;
-			}
-		return new int[] { min, max };
-	}
-
-	private int[] calcMinMax(StoredValue storedValue, ComponentSampleModel sm,
-			short[] data) {
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		int w = sm.getWidth();
-		int h = sm.getHeight();
-		int stride = sm.getScanlineStride();
-		for (int y = 0; y < h; y++)
-			for (int i = y * stride, end = i + w; i < end;) {
-				int val = storedValue.valueOf(data[i++]);
-				if (val < min) min = val;
-				if (val > max) max = val;
-			}
-		return new int[] { min, max };
-	}
-
-	public LookupTable createSigmoidLUT(StoredValue inBits,
-			int outBits){
-
-		int outRange = 1 << outBits;
-
-		float c = windowCenter;
-		float w = windowWidth;
-		float b = rescaleIntercept;
-		float m = rescaleSlope;
-		float k = -4 * m / w;
-		int size = inBits.maxValue(); //LUT
-		int offset = 0; //Need to use no offset, not necessary for linear LUT
-
-		float ic = c;
-
-		ic = (c - b) / m -offset;
-
-		if(outBits > 8){
-			short[] lut = new short[size];
-			for (int i = 0; i < size; i++){
-
-				int tmp = calcSigmoid(outRange, i, ic, k);
-				
-				lut[i] = (short) (tmp);
-			}
-
-			return new ShortLookupTable(inBits, outBits, 0, lut);
-		} else {
-			byte[] lut = new byte[size];
-
-			for (int i = 0; i < size; i++){
-
-				int tmp = calcSigmoid(outRange, i, ic, k);
-
-				lut[i] = (byte) ((tmp) & 0xff);
-
-			}
-
-			return new ByteLookupTable(inBits, outBits, 0, lut);
-
-		}
-
-	}
-
-	private int calcSigmoid(int outRange, float i, float ic, float k){
-		double num = (1 + Math.exp((i - ic) * k));
-
-		return (int) (outRange / num);
+	return (int) (outRange / num);
 
 
-	}
+    }
 
 }

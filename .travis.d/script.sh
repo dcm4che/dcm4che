fd532a9a5a471bf647a68c780fe7333e35602949
Christopher Redekop <chris.redekop@gmail.com>
2014-11-17 14:07:58 -0500

    [#LIB-247] Implement Continuous Delivery for dcm4che; not checking for master revision until we know we are on the master branch.

--- .travis.d/script.sh
+++ .travis.d/script.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 set -ev
 
-MASTER_COMMIT=$(git rev-parse master)
+doTest() {
+  echo "The current commit is NOT a release candidate: attempt to test."
+  mvn verify
+}
 
 # Only release from master if
 # A) we know of no commits since this build's commit and
 # B) this build is not a pull request and
 # C) secure variables are available
 if [[ ${TRAVIS_BRANCH} == "master" && \
-  ${TRAVIS_COMMIT} == ${MASTER_COMMIT} && \
   ${TRAVIS_PULL_REQUEST} == "false" && \
   ${TRAVISE_SECURE_ENV_VARS} == "true" ]]
 then
-  echo "The current commit is a release candidate: attempt to release."
+    MASTER_COMMIT=$(git rev-parse master)
+
+    if [[ ${TRAVIS_COMMIT} == ${MASTER_COMMIT} ]]
+    then
+      echo "The current commit is a release candidate: attempt to release."
   
-  # Make credentials available
-  openssl aes-256-cbc -K $encrypted_4269c1dbcc16_key -iv $encrypted_4269c1dbcc16_iv \
-    -in .travis.d/secret.tar.enc -out .travis.d/secret.tar -d
-  tar xf .travis.d/secret.tar -C .travis.d
+      # Make credentials available
+      openssl aes-256-cbc -K $encrypted_4269c1dbcc16_key -iv $encrypted_4269c1dbcc16_iv \
+        -in .travis.d/secret.tar.enc -out .travis.d/secret.tar -d
+      tar xf .travis.d/secret.tar -C .travis.d
 
-  # Setup git clone for committing release POMs
-  export GIT_SSH=${TRAVIS_BUILD_DIR}/.travis.d/ssh.sh
-  git config --global user.name "Christopher Redekop"
-  git config --global user.email "chris.redekop@gmail.com"
-  git checkout master
+      # Setup git clone for committing release POMs
+      export GIT_SSH=${TRAVIS_BUILD_DIR}/.travis.d/ssh.sh
+      git config --global user.name "Christopher Redekop"
+      git config --global user.email "chris.redekop@gmail.com"
+      git checkout master
 
-  # Run the release
-  mvn -s .travis.d/settings.xml -B release:prepare
-  mvn -s .travis.d/settings.xml -P travis-release-perform release:perform 
+      # Run the release
+      mvn -s .travis.d/settings.xml -B release:prepare
+      mvn -s .travis.d/settings.xml -P travis-release-perform release:perform 
+    else
+      doTest
+    fi
 else
-  echo "The current commit is NOT a release candidate: attempt to  test."
-  mvn verify
+  doTest
 fi

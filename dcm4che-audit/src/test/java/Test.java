a5a4880ae5848a0e8d4c5c771ba9a5474e50158b
gunterze <gunterze@gmail.com>
2014-11-26 08:57:23 +0100

    test

--- dcm4che-audit/src/test/java/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-
-    @org.junit.Test
-    public void test() {
-        System.out.println(System.getenv("encrypted_4269c1dbcc16_key"));
-        System.out.println(System.getenv("encrypted_4269c1dbcc16_iv"));
-    }
-
-}

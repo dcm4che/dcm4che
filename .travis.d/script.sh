6dc7d32dee8a16a6347e0aab388c3353bca1c0d9
Christopher Redekop <chris.redekop@gmail.com>
2014-12-02 09:29:29 -0500

    [#LIB-247] Implement Continuous Delivery for dcm4che; fix a typo and improve logging.

--- .travis.d/script.sh
+++ .travis.d/script.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -ev
 
-doTest() {
-  echo "The current commit is NOT a release candidate: attempt to test."
-  mvn verify
-}
-
 echo "TRAVIS_BRANCH = '${TRAVIS_BRANCH}'"
 echo "TRAVIS_PULL_REQUEST = '${TRAVIS_PULL_REQUEST}'"
 echo "TRAVIS_SECURE_ENV_VARS = '${TRAVIS_SECURE_ENV_VARS}'"
@@ -16,7 +11,7 @@ echo "TRAVIS_SECURE_ENV_VARS = '${TRAVIS_SECURE_ENV_VARS}'"
 # C) secure variables are available
 if [[ ${TRAVIS_BRANCH} == "master" && \
   ${TRAVIS_PULL_REQUEST} == "false" && \
-  ${TRAVISE_SECURE_ENV_VARS} == "true" ]]
+  ${TRAVIS_SECURE_ENV_VARS} == "true" ]]
 then
     MASTER_COMMIT=$(git ls-remote origin master | cut -f 1)
 
@@ -25,7 +20,7 @@ then
 
     if [[ ${TRAVIS_COMMIT} == ${MASTER_COMMIT} ]]
     then
-      echo "The current commit is a release candidate: attempt to release."
+      echo "The current commit is the latest release candidate: attempt to release."
   
       # Make credentials available
       openssl aes-256-cbc -K $encrypted_1fc90f464345_key -iv $encrypted_1fc90f464345_iv \
@@ -40,8 +35,10 @@ then
       mvn -s .travis.d/settings.xml versions:set -DnewVersion='${BUILD_VERSION}'
       mvn -s .travis.d/settings.xml -P ossrh,travis-secret deploy -Dscm.revision="${TRAVIS_COMMIT}"
     else
-      doTest
+      echo "A later release candidate has been committed: attempt to verify."
+      mvn verify
     fi
 else
-  doTest
+  echo "The current commit is not a release candidate: attempt to verify."
+  mvn verify
 fi

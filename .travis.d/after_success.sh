12ed0c0acdaf922c100252eb1f6f2130151f2e13
Christopher Redekop <chris.redekop@gmail.com>
2014-12-04 17:55:57 -0500

    [#LIB-247] Implement Continuous Delivery for dcm4che; correcting the build propagation.

--- .travis.d/after_success.sh
+++ .travis.d/after_success.sh
@@ -3,12 +3,10 @@ set -ev
 
 if [[ ${RELEASE_CANDIDATE} == "true" ]]
 then
-  BUILD_ID=$(curl -s -H 'Accept: application/vnd.travis-ci.2+json' \
-    https://api.travis-ci.org/repos/dcm4che/dcm4chee-storage2/builds \
-    | grep -o '^{\"builds\":\\[{\"id\":[0-9]*' | grep -o '[0-9]*' | tr -d '\\n')
+  BUILD_ID=$(curl -s -H 'Accept: application/vnd.travis-ci.2+json' https://api.travis-cios/dcm4che/dcm4chee-storage2/builds | grep -o '^{"builds":\[{"id":[0-9]*' | grep -o '[0-9]*' | tr -d '\n')
 
   echo "BUILD_ID = ${BUILD_ID}"
 
-  curl -H \"Authorization: token ${TRAVIS_TOKEN}\" -H 'Accept: application/vnd.travis-ci.2+json' -X POST \
+  curl -H "Authorization: token ${TRAVIS_TOKEN}" -H 'Accept: application/vnd.travis-ci.2+json' -X POST \
     https://api.travis-ci.org/builds/${BUILD_ID}/restart
 fi

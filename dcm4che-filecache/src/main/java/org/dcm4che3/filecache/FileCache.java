05bd9d6e10d78ad788438f7e07cae28a8434cd7b
gunterze <gunterze@gmail.com>
2014-12-19 16:24:16 +0100

    [LIB-279] Provide LRU File Cache

---
+++ dcm4che-filecache/src/main/java/org/dcm4che3/filecache/FileCache.java
@@ -0,0 +1,367 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/gunterze/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Agfa Healthcare.
+ * Portions created by the Initial Developer are Copyright (C) 2011-2014
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+package org.dcm4che3.filecache;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Gunter Zeilinger <gunterze@gmail.com>
+ *
+ */
+public class FileCache {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FileCache.class);
+    private static final Charset UTF_8 = Charset.forName("UTF-8");
+
+    private Path fileCacheRootDirectory;
+    private Path journalRootDirectory;
+    private String journalFileName = "journal";
+    private String orphanedFileName = "orphaned";
+    private String journalDirectoryName = "journal.d";
+    private SimpleDateFormat journalFileNamePattern =
+            new SimpleDateFormat("yyyyMMDDHHmmss");
+    private int journalFileSize = 100;
+    private boolean leastRecentlyUsed;
+    private int currentJournalFileSize = -1;
+    private final AtomicBoolean freeIsRunning = new AtomicBoolean();
+
+    public Path getFileCacheRootDirectory() {
+        return fileCacheRootDirectory;
+    }
+
+    public void setFileCacheRootDirectory(Path fileCacheRootDirectory) {
+        this.fileCacheRootDirectory = fileCacheRootDirectory;
+    }
+
+    public Path getJournalRootDirectory() {
+        return journalRootDirectory;
+    }
+
+    public void setJournalRootDirectory(Path journalRootDirectory) {
+        this.journalRootDirectory = journalRootDirectory;
+    }
+
+    public String getJournalFileName() {
+        return journalFileName;
+    }
+
+    public void setJournalFileName(String journalFileName) {
+        this.journalFileName = journalFileName;
+    }
+
+    public Path getJournalFile() {
+        return journalRootDirectory.resolve(journalFileName);
+    }
+
+    public String getJournalDirectoryName() {
+        return journalDirectoryName;
+    }
+
+    public void setJournalDirectoryName(String journalDirectoryName) {
+        this.journalDirectoryName = journalDirectoryName;
+    }
+
+    public Path getJournalDirectory() {
+        return journalRootDirectory.resolve(journalDirectoryName);
+    }
+
+    public String getJournalFileNamePattern() {
+        return journalFileNamePattern.toPattern();
+    }
+
+    public void setJournalFileNamePattern(String pattern) {
+        this.journalFileNamePattern = new SimpleDateFormat(pattern);
+    }
+
+    public int getJournalFileSize() {
+        return journalFileSize;
+    }
+
+    public void setJournalFileSize(int journalFileSize) {
+        this.journalFileSize = journalFileSize;
+    }
+
+    public String getOrphanedFileName() {
+        return orphanedFileName;
+    }
+
+    public void setOrphanedFileName(String orphanedFileName) {
+        this.orphanedFileName = orphanedFileName;
+    }
+
+    public Path getOrphanedFile() {
+        return journalRootDirectory.resolve(orphanedFileName);
+    }
+
+    public Collection<Path> getOrphanedFiles() throws IOException {
+        Path orphanFile = getOrphanedFile();
+        if (Files.notExists(orphanFile))
+            return Collections.emptyList();
+        
+        ArrayList<Path> files = new ArrayList<Path>();
+        try (BufferedReader r = Files.newBufferedReader(
+                orphanFile, UTF_8)) {
+            String fileName;
+            while ((fileName = r.readLine()) != null) {
+                files.add(fileCacheRootDirectory.resolve(fileName));
+            }
+        }
+        return files;
+    }
+
+    public boolean isLeastRecentlyUsed() {
+        return leastRecentlyUsed;
+    }
+
+    public void setLeastRecentlyUsed(boolean leastRecentlyUsed) {
+        this.leastRecentlyUsed = leastRecentlyUsed;
+    }
+
+    @Override 
+    public String toString() {
+        return "FileCache[cacheDir=" + fileCacheRootDirectory
+                + ", journalDir=" + journalRootDirectory + "]";
+    }
+
+    public synchronized void register(Path path) throws IOException {
+        LOG.debug("{}: registering - {}", this, path);
+        Files.createDirectories(journalRootDirectory);
+        Path journalFile = getJournalFile();
+        String entry = fileCacheRootDirectory.relativize(path).toString();
+        int size = currentJournalFileSize;
+        if (size < 0)
+            size = countLines(journalFile);
+        if (size >= journalFileSize) {
+            FileTime journalFileTime = Files.getLastModifiedTime(journalFile);
+            Path dir = getJournalDirectory();
+            Path target = dir.resolve(journalFileNamePattern.format(
+                    new Date( journalFileTime.toMillis())));
+            LOG.debug("{}: journalFileSize[{}] exeeded, move {} to {}", 
+                    this, journalFileSize, journalFile, target);
+            Files.createDirectories(dir);
+            Files.move(journalFile, target);
+            size = 0;
+        }
+        Files.write(journalFile, Collections.singleton(entry), UTF_8,
+                StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        currentJournalFileSize = size + 1;
+        if (leastRecentlyUsed) {
+            try {
+                LOG.debug("{}: update modification time of - {}", this, path);
+                Files.setLastModifiedTime(path, Files.getLastModifiedTime(journalFile));
+            } catch (IOException e) {
+                LOG.info("{}: failed to update modification time of - {}", this, path, e);
+            }
+        }
+        LOG.debug("{}: registered - {}", this, path);
+    }
+
+    public long free(long size) throws IOException {
+        LOG.info("{}: try to free {} bytes", this, size);
+        if (!freeIsRunning.compareAndSet(false, true)) {
+            LOG.info("{}: free already running", this);
+            return -1;
+        }
+
+        try {
+            long freed = 0L;
+            SortedSet<PathFileTime> journalFiles = listOldJournalFiles();
+            for (PathFileTime journalFile : journalFiles) {
+                try {
+                    freed += free(journalFile);
+                    if (freed >= size)
+                        break;
+                } catch (IOException e) {
+                    LOG.warn("{}: failed deleting files referenced by journal - {}",
+                            this, journalFile.path);
+                }
+            }
+            LOG.info("{}: freed {} bytes", this, freed);
+            return freed;
+        } finally {
+            freeIsRunning.set(false);
+        }
+    }
+
+    public void clear() throws IOException {
+        LOG.info("{}: clearing", this);
+        deleteDirContent(fileCacheRootDirectory);
+        deleteDirContent(journalRootDirectory);
+        LOG.info("{}: cleared", this);
+    }
+
+    private int countLines(Path journalFile) throws IOException {
+        int lines = 0;
+        if (Files.exists(journalFile))
+            try (BufferedReader r = Files.newBufferedReader(journalFile, UTF_8)) {
+                while (r.readLine() == null)
+                    lines ++;
+            }
+        return lines;
+    }
+
+    private static void deleteDirContent(Path dir) throws IOException {
+        if (Files.exists(dir)) {
+            try ( DirectoryStream<Path> in = Files.newDirectoryStream(dir) ) {
+                for (Path path : in) {
+                    if (Files.isDirectory(path))
+                        deleteDirContent(path);
+                    Files.delete(path);
+                }
+            }
+        }
+    }
+
+    private SortedSet<PathFileTime> listOldJournalFiles() throws IOException {
+        TreeSet<PathFileTime> files = new TreeSet<PathFileTime>();
+        Path dir = getJournalDirectory();
+        if (Files.isDirectory(dir))
+            try (DirectoryStream<Path> dirPath = Files.newDirectoryStream(dir)) {
+                for (Path path : dirPath) {
+                    files.add(new PathFileTime(path, Files.getLastModifiedTime(path)));
+                }
+            }
+        return files;
+    }
+
+    private long free(PathFileTime journalFile) throws IOException {
+        LOG.debug("{}: deleting files referenced by journal - {}",
+                this, journalFile.path);
+        long freed = 0L;
+        try (BufferedReader r = Files.newBufferedReader(
+                journalFile.path, UTF_8)) {
+            String fileName;
+            while ((fileName = r.readLine()) != null) {
+                Path path = fileCacheRootDirectory.resolve(fileName);
+                if (Files.notExists(path)) {
+                    LOG.debug("{}: {} already deleted");
+                    continue;
+                }
+                if (leastRecentlyUsed) {
+                    try {
+                        if (Files.getLastModifiedTime(path)
+                                .compareTo(journalFile.time) > 0)  {
+                            LOG.debug("{}: {} recently accessed - do not delete",
+                                    this, path);
+                            continue;
+                        }
+                    } catch (IOException e) {
+                        LOG.info("{}: failed to get modification time of - {}",
+                                this, path, e);
+                    }
+                }
+                try {
+                    LOG.debug("{}: delete - {}", this, path);
+                    long fileSize = Files.size(path);
+                    Files.delete(path);
+                    freed += fileSize;
+                    purgeEmptyDirectories(path);
+                } catch (IOException e) {
+                    LOG.warn("{}: failed to delete - {}", this, path, e);
+                    try {
+                        Path orphanedFile = getOrphanedFile();
+                        Files.write(orphanedFile, Collections.singleton(fileName), UTF_8,
+                                StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+                    } catch (IOException e2) {
+                        LOG.warn("{}: failed to record orphaned file - {}",
+                                this, path, e2);
+                    }
+                }
+            }
+        }
+        try {
+            LOG.debug("{}: delete journal - {}", this, journalFile.path);
+            Files.delete(journalFile.path);
+        } catch (IOException e) {
+            LOG.warn("{}: failed to delete journal - {}", this, journalFile.path, e);
+        }
+        LOG.debug("{}: deleted files referenced by journal - {} - freed {} bytes",
+                this, journalFile.path, freed);
+        return freed;
+    }
+
+    private void purgeEmptyDirectories(Path path) {
+        while (!(path = path.getParent()).equals(fileCacheRootDirectory))
+            try {
+                Files.delete(path);
+                LOG.debug("{}: purged empty directory - {}", this, path);
+            } catch (DirectoryNotEmptyException e) {
+                return;
+            } catch (IOException e) {
+                LOG.warn("{}: failed to purge empty directory {}", this, path, e);
+            }
+    }
+
+    private static final class PathFileTime implements Comparable<PathFileTime> {
+
+        final Path path;
+        final FileTime time;
+
+        PathFileTime(Path path, FileTime time) {
+            this.path = path;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(PathFileTime o) {
+            return time.compareTo(o.time);
+        }
+
+    }
+
+}
---
+++ dcm4che-filecache/src/test/java/org/dcm4che3/filecache/FileCacheTest.java
@@ -0,0 +1,161 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/gunterze/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Agfa Healthcare.
+ * Portions created by the Initial Developer are Copyright (C) 2011-2014
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+package org.dcm4che3.filecache;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Iterator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Gunter Zeilinger <gunterze@gmail.com>
+ *
+ */
+public class FileCacheTest {
+
+    private static final Charset UTF_8 = Charset.forName("UTF-8");
+    private static final byte[] BYTES = { 'b', 'y', 't', 'e', 's' };
+    private static final String[] FILES1 = { "a", "b/c" };
+    private static final String[] FILES2 = { "d", "e" };
+    private static final String[] FILES3 = { "f", "a" };
+    private static final String[] DELETED = { "a", "b/c", "b" };
+    private static final String[] DELETED_LRU = { "b/c", "b", "d", "e" };
+    private static final String[] NOT_DELETED = { "d", "e", "f" };
+    private static final String[] NOT_DELETED_LRU = FILES3;
+    private static final long FREED = BYTES.length * 2;
+    private static final long FREED_LRU = BYTES.length * 3;
+    private static final Path CACHE_ROOT_DIR = Paths.get("target/filecache");
+    private static final Path JOURNAL_ROOT_DIR = Paths.get("target/journaldir");
+    private static final long DELAY = 1000L;
+    
+    private FileCache fileCache = new FileCache();
+
+    @Before
+    public void setUp() throws Exception {
+        fileCache.setFileCacheRootDirectory(CACHE_ROOT_DIR);
+        fileCache.setJournalRootDirectory(JOURNAL_ROOT_DIR);
+        fileCache.setJournalFileSize(2);
+        fileCache.clear();
+    }
+
+    @Test
+    public void testRegister() throws Exception {
+        registerFiles();
+        assertEquals(Arrays.asList(FILES3), 
+                Files.readAllLines(fileCache.getJournalFile(), UTF_8));
+        try (DirectoryStream<Path> dir =
+                Files.newDirectoryStream(fileCache.getJournalDirectory())) {
+            Iterator<Path> iter = dir.iterator();
+            assertTrue(iter.hasNext());
+            Path path1 = iter.next();
+            assertTrue(iter.hasNext());
+            Path path2 = iter.next();
+            assertFalse(iter.hasNext());
+            if (Files.getLastModifiedTime(path1)
+                    .compareTo(Files.getLastModifiedTime(path2)) > 0) {
+                Path tmp = path1;
+                path1 = path2;
+                path2 = tmp;
+            }
+            assertEquals(Arrays.asList(FILES1) , Files.readAllLines(path1, UTF_8));
+            assertEquals(Arrays.asList(FILES2) , Files.readAllLines(path2, UTF_8));
+        }
+    }
+
+    private void registerFiles() throws Exception {
+        for (String file : FILES1)
+            fileCache.register(createFile(file));
+        Thread.sleep(DELAY);
+        for (String file : FILES2)
+            fileCache.register(createFile(file));
+        Thread.sleep(DELAY);
+        for (String file : FILES3)
+            fileCache.register(createFile(file));
+    }
+
+    private Path createFile(String file) throws IOException {
+        Path path = toPath(file);
+        Files.createDirectories(path.getParent());
+        Files.write(path, BYTES);
+        return path;
+    }
+
+    private Path toPath(String file) {
+        return CACHE_ROOT_DIR.resolve(file);
+    }
+
+    @Test
+    public void testFree() throws Exception {
+        registerFiles();
+        assertEquals(FREED, fileCache.free(FREED));
+        assertNotExists(DELETED);
+        assertExists(NOT_DELETED);
+    }
+
+    @Test
+    public void testFreeLRU() throws Exception {
+        fileCache.setLeastRecentlyUsed(true);
+        registerFiles();
+        assertEquals(FREED_LRU, fileCache.free(FREED));
+        assertNotExists(DELETED_LRU);
+        assertExists(NOT_DELETED_LRU);
+    }
+
+    private void assertNotExists(String[] files) {
+        for (String file : files)
+            assertTrue(Files.notExists(toPath(file)));
+    }
+
+    private void assertExists(String[] files) {
+        for (String file : files)
+            assertTrue(Files.exists(toPath(file)));
+    }
+
+}

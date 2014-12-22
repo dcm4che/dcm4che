b10ddcff6574653b7ce564dd1b51d49688a71142
daniela-radu <radu.daniela@gmail.com>
2014-12-22 16:23:55 +0100

    Correct xml space preserve
    The correct form for specifying to the application that all white space in an xml should be preserved is xml:space="preserve" and not  xml-space="preserved". For more details on this please visit http://www.w3.org/TR/xml11/#sec-white-space
    (cherry picked from commit e279cd7)

--- dcm4che-core/src/main/java/org/dcm4che3/io/SAXWriter.java
+++ dcm4che-core/src/main/java/org/dcm4che3/io/SAXWriter.java
@@ -139,7 +139,7 @@ public class SAXWriter implements DicomInputHandler {
 
     private void startDocument() throws SAXException {
         ch.startDocument();
-        startElement("NativeDicomModel", "xml-space", "preserved");
+        startElement("NativeDicomModel", "xml:space", "preserve");
     }
 
     private void endDocument() throws SAXException {

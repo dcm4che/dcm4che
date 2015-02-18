5f267e0f337f6e771379dfa231b7cc58760dd0d9
gunterze <gunterze@gmail.com>
2015-02-18 12:31:15 +0100

    [LIB-297] Upgrade to DICOM 2015a release [LIB-301] Support Value Representation Unlimited Characters (UC) according CP 1031 [LIB-302] Create Value Representation Universal Resource Identifier or Universal Resource Locator (UR) according CP 1324 (cherry picked from commit 70d45bb)

--- dcm4che-json/src/main/java/org/dcm4che3/json/JSONWriter.java
+++ dcm4che-json/src/main/java/org/dcm4che3/json/JSONWriter.java
@@ -189,7 +189,9 @@ public class JSONWriter implements DicomInputHandler {
         case SH:
         case ST:
         case TM:
+        case UC:
         case UI:
+        case UR:
         case UT:
             writeStringValues(vr, val, bigEndian, cs);
             break;

e1e339c6fe25ac0e399e165058ec0a7b071fd999
romank-agfa <roman.khazankin@agfa.com>
2015-03-28 14:03:08 +0100

    [DCMEEREQ-300] Configuration API | Pre-integration refactoring

--- dcm4che-conf/dcm4che-conf-core/src/main/java/org/dcm4che3/conf/api/DicomConfigurationCustomizer.java
+++ dcm4che-conf/dcm4che-conf-core/src/main/java/org/dcm4che3/conf/api/DicomConfigurationCustomizer.java
@@ -4,6 +4,8 @@ import org.dcm4che3.conf.dicom.DicomConfigurationBuilder;
 
 /**
  * Vendors may implement this interface to perform necessary modification to the configuration bootstrap process, e.g., set custom storage, add custom extensions.
+ *
+ * This API is UNSTABLE and can be changed without notice, please do not use it without prior consulting.
  */
 public interface DicomConfigurationCustomizer {
 

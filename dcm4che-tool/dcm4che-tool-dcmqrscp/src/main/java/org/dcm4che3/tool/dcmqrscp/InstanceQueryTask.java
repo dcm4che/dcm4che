7a93a228c7868f100fbb9cdd0225f8092d64fefe
Hermann Czedik-Eysenberg <hermann.czedik-eysenberg@agfa.com>
2015-06-24 15:18:28 +0200

    Fix squid:S00113 - Files should contain an empty new line at the end

--- dcm4che-tool/dcm4che-tool-dcmqrscp/src/main/java/org/dcm4che3/tool/dcmqrscp/InstanceQueryTask.java
+++ dcm4che-tool/dcm4che-tool-dcmqrscp/src/main/java/org/dcm4che3/tool/dcmqrscp/InstanceQueryTask.java
@@ -40,8 +40,8 @@ package org.dcm4che3.tool.dcmqrscp;
 
 import java.io.IOException;
 
-import org.dcm4che3.data.Tag;
 import org.dcm4che3.data.Attributes;
+import org.dcm4che3.data.Tag;
 import org.dcm4che3.media.DicomDirReader;
 import org.dcm4che3.net.Association;
 import org.dcm4che3.net.Status;

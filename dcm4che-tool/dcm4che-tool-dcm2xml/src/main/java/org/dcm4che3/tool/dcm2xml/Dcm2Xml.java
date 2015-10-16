f28692ca165a44e04efc8c9649320d17a2a9dfca
Gunter Zeilinger <gunterze@gmail.com>
2015-10-16 15:12:29 +0200

    [LIB-405] dcm2xml: Support to specify XSLT stylesheet also by URL
    Conflicts:
    	dcm4che-tool/dcm4che-tool-dcm2xml/src/main/java/org/dcm4che3/tool/dcm2xml/Dcm2Xml.java

--- dcm4che-tool/dcm4che-tool-dcm2xml/src/main/java/org/dcm4che3/tool/dcm2xml/Dcm2Xml.java
+++ dcm4che-tool/dcm4che-tool-dcm2xml/src/main/java/org/dcm4che3/tool/dcm2xml/Dcm2Xml.java
@@ -43,6 +43,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -84,7 +86,7 @@ public class Dcm2Xml {
     private static ResourceBundle rb =
         ResourceBundle.getBundle("org.dcm4che3.tool.dcm2xml.messages");
 
-    private File xsltFile;
+    private String xsltURL;
     private boolean indent = false;
     private boolean includeKeyword = true;
     private boolean includeNamespaceDeclaration = false;
@@ -96,8 +98,8 @@ public class Dcm2Xml {
     private Attributes blkAttrs;
     private String xmlVersion = XML_1_0;
 
-    public final void setXSLT(File xsltFile) {
-        this.xsltFile = xsltFile;
+    public final void setXSLTURL(String xsltURL) {
+        this.xsltURL = xsltURL;
     }
 
     public final void setIndent(boolean indent) {
@@ -206,7 +208,7 @@ public class Dcm2Xml {
             CommandLine cl = parseComandLine(args);
             Dcm2Xml main = new Dcm2Xml();
             if (cl.hasOption("x"))
-                main.setXSLT(new File(cl.getOptionValue("x")));
+                main.setXSLTURL(toURL(cl.getOptionValue("x")));
             main.setIndent(cl.hasOption("I"));
             main.setIncludeKeyword(!cl.hasOption("K"));
             main.setIncludeNamespaceDeclaration(cl.hasOption("xmlns")); if (cl.hasOption("xml11"))
@@ -226,6 +228,15 @@ public class Dcm2Xml {
             System.err.println("dcm2xml: " + e.getMessage());
             e.printStackTrace();
             System.exit(2);
+        }
+    }
+
+    private static String toURL(String fileOrURL) {
+        try {
+            new URL(fileOrURL);
+            return fileOrURL;
+        } catch (MalformedURLException e) {
+            return new File(fileOrURL).toURI().toString();
         }
     }
 
@@ -319,11 +330,11 @@ public class Dcm2Xml {
             throws TransformerConfigurationException, IOException {
         SAXTransformerFactory tf = (SAXTransformerFactory)
                 TransformerFactory.newInstance();
-        if (xsltFile == null)
+        if (xsltURL == null)
             return tf.newTransformerHandler();
 
         TransformerHandler th = tf.newTransformerHandler(
-                new StreamSource(xsltFile));
+                new StreamSource(xsltURL));
         return th;
     }
 }

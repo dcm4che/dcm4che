f197370a8c35da2ca8096f8d12abeb98437bf64f
Umberto Cappellini <umberto.cappellini@agfa.com>
2015-11-27 15:58:41 +0100

    [DCMEEREQ-1158] DeIdentification doesn't work for C-Find and C-Store

--- dcm4che-core/src/main/java/org/dcm4che3/data/Attributes.java
+++ dcm4che-core/src/main/java/org/dcm4che3/data/Attributes.java
@@ -55,10 +55,7 @@ import org.dcm4che3.io.BulkDataDescriptor;
 import org.dcm4che3.io.DicomEncodingOptions;
 import org.dcm4che3.io.DicomInputStream;
 import org.dcm4che3.io.DicomOutputStream;
-import org.dcm4che3.util.ByteUtils;
-import org.dcm4che3.util.DateUtils;
-import org.dcm4che3.util.StringUtils;
-import org.dcm4che3.util.TagUtils;
+import org.dcm4che3.util.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -2449,31 +2446,31 @@ public class Attributes implements Serializable {
         return toString(TO_STRING_LIMIT, TO_STRING_WIDTH);
     }
 
-    public String toString(boolean deidentify) {
-        return toString(TO_STRING_LIMIT, TO_STRING_WIDTH, deidentify);
+    public String toString(Deidentifier deidentifier) {
+        return toString(TO_STRING_LIMIT, TO_STRING_WIDTH, deidentifier);
     }
 
     public String toString(int limit, int maxWidth) {
-        return toStringBuilder(limit, maxWidth, new StringBuilder(1024), false)
+        return toStringBuilder(limit, maxWidth, new StringBuilder(1024), null)
                 .toString();
     }
 
-    public String toString(int limit, int maxWidth, boolean deidentify) {
-        return toStringBuilder(limit, maxWidth, new StringBuilder(1024), deidentify)
+    public String toString(int limit, int maxWidth, Deidentifier deidentifier) {
+        return toStringBuilder(limit, maxWidth, new StringBuilder(1024), deidentifier)
                 .toString();
     }
 
     public StringBuilder toStringBuilder(StringBuilder sb) {
-        return toStringBuilder(TO_STRING_LIMIT, TO_STRING_WIDTH, sb, false);
+        return toStringBuilder(TO_STRING_LIMIT, TO_STRING_WIDTH, sb, null);
     }
 
-    public StringBuilder toStringBuilder(int limit, int maxWidth, StringBuilder sb, boolean deidentify) {
-        if (appendAttributes(limit, maxWidth, sb, "", deidentify) > limit)
+    public StringBuilder toStringBuilder(int limit, int maxWidth, StringBuilder sb, Deidentifier deidentifier) {
+        if (appendAttributes(limit, maxWidth, sb, "", deidentifier) > limit)
             sb.append("...\n");
         return sb;
     }
 
-    private int appendAttributes(int limit, int maxWidth, StringBuilder sb, String prefix, boolean deidentify) {
+    private int appendAttributes(int limit, int maxWidth, StringBuilder sb, String prefix, Deidentifier deidentifier) {
         int lines = 0;
         int creatorTag = 0;
         String privateCreator = null;
@@ -2492,30 +2489,30 @@ public class Attributes implements Serializable {
                 privateCreator = null;
             }
             Object value = values[i];
-            appendAttribute(privateCreator, tag, vrs[i], value, sb.length() + maxWidth, sb, prefix, deidentify);
+            appendAttribute(privateCreator, tag, vrs[i], value, sb.length() + maxWidth, sb, prefix, deidentifier);
             if (value instanceof Sequence)
-                lines += appendItems((Sequence) value, limit - lines, maxWidth, sb, prefix + '>', deidentify);
+                lines += appendItems((Sequence) value, limit - lines, maxWidth, sb, prefix + '>', deidentifier);
         }
         return lines;
     }
 
     private int appendItems(Sequence sq, int limit, int maxWidth, StringBuilder sb,
-            String prefix, boolean deidentify) {
+            String prefix, Deidentifier deidentifier) {
         int lines = 0;
         int itemNo = 0;
         for (Attributes item : sq) {
             if (++lines > limit)
                 break;
             sb.append(prefix).append("Item #").append(++itemNo).append('\n');
-            lines += item.appendAttributes(limit - lines, maxWidth, sb, prefix, deidentify);
+            lines += item.appendAttributes(limit - lines, maxWidth, sb, prefix, deidentifier);
         }
         return lines ;
     }
 
     private StringBuilder appendAttribute(String privateCreator, int tag, VR vr, Object value,
-            int maxLength, StringBuilder sb, String prefix, boolean deidentify) {
+            int maxLength, StringBuilder sb, String prefix, Deidentifier deidentifier) {
         sb.append(prefix).append(TagUtils.toString(tag)).append(' ').append(vr).append(" [");
-        if (vr.prompt((deidentify ? deidentify(tag,vr,value):value), bigEndian,
+        if (vr.prompt((deidentifier!=null ? deidentifier.deidentify(tag, vr, value) :value), bigEndian,
                 getSpecificCharacterSet(vr),
                 maxLength - sb.length() - 1, sb)) {
             sb.append("] ").append(ElementDictionary.keywordOf(tag, privateCreator));
@@ -2524,17 +2521,6 @@ public class Attributes implements Serializable {
         }
         sb.append('\n');
         return sb;
-    }
-
-    private Object deidentify(int tag, VR vr, Object value) {
-        if (tag == Tag.PatientBirthDate ||
-            tag == Tag.PatientSex ||
-            tag == Tag.PatientAge ||
-            tag == Tag.PatientAddress ||
-            vr.equals(VR.PN))
-            return "XXX";
-        else
-            return value;
     }
 
     public int calcLength(DicomEncodingOptions encOpts, boolean explicitVR) {

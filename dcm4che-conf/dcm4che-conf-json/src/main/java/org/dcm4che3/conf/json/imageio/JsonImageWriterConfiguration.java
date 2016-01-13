ce1f4dea62634a78683b607f5dcc23920ef6a7cc
vrindanayak <vrinda.nayak@j4care.com>
2016-01-13 16:19:17 +0100

    Fix dcm4che/dcm4chee-arc-light#5: Provide RESTful Service to access configuration in LDAP

--- dcm4che-conf/dcm4che-conf-json/src/main/java/org/dcm4che3/conf/json/imageio/JsonImageWriterConfiguration.java
+++ dcm4che-conf/dcm4che-conf-json/src/main/java/org/dcm4che3/conf/json/imageio/JsonImageWriterConfiguration.java
@@ -2,11 +2,15 @@ package org.dcm4che3.conf.json.imageio;
 
 import org.dcm4che3.conf.json.JsonConfigurationExtension;
 import org.dcm4che3.conf.json.JsonWriter;
+import org.dcm4che3.conf.json.JsonReader;
+import org.dcm4che3.imageio.codec.ImageReaderFactory;
 import org.dcm4che3.imageio.codec.ImageWriterFactory;
 import org.dcm4che3.net.Device;
+import org.dcm4che3.net.imageio.ImageReaderExtension;
 import org.dcm4che3.net.imageio.ImageWriterExtension;
-import org.dcm4che3.util.Property;
 
+import javax.json.stream.JsonParser;
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
@@ -38,5 +42,45 @@ public class JsonImageWriterConfiguration extends JsonConfigurationExtension {
         writer.writeEnd();
     }
 
+    @Override
+    public boolean loadDeviceExtension(Device device, JsonReader reader) {
+        if (!reader.getString().equals("dcmImageWriter"))
+            return false;
 
+        ImageWriterFactory factory = new ImageWriterFactory();
+        reader.next();
+        reader.expect(JsonParser.Event.START_ARRAY);
+        while (reader.next() == JsonParser.Event.START_OBJECT) {
+            String tsuid = null;
+            String formatName = null;
+            String className = null;
+            String patchJPEGLS = null;
+            String[] imageWriteParam = new String[0];
+            while (reader.next() == JsonParser.Event.KEY_NAME) {
+                switch (reader.getString()) {
+                    case "dicomTransferSyntax":
+                        tsuid = reader.stringValue();
+                        break;
+                    case "dcmIIOFormatName":
+                        formatName = reader.stringValue();
+                        break;
+                    case "dcmJavaClassName":
+                        className = reader.stringValue();
+                        break;
+                    case "dcmPatchJPEGLS":
+                        patchJPEGLS = reader.stringValue();
+                        break;
+                    case "dcmImageWriteParam":
+                        imageWriteParam = reader.stringArray();
+                        break;
+                    default:
+                        reader.skipUnknownProperty();
+                }
+            }
+            reader.expect(JsonParser.Event.END_OBJECT);
+            factory.put(tsuid, new ImageWriterFactory.ImageWriterParam(formatName, className, patchJPEGLS, imageWriteParam));
+        }
+        device.addDeviceExtension(new ImageWriterExtension(factory));
+        return true;
+    }
 }

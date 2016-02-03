dd229a68f2efa323721b163cb55df47d2a2fcdc9
vrindanayak <vrinda.nayak@j4care.com>
2016-02-03 10:27:36 +0100

    dcm4che/dcm4chee-arc-light#34: Reverted the changes done for - Add network connection on device fails if Protocol not equal to DICOM

--- dcm4che-net/src/main/java/org/dcm4che3/net/ApplicationEntity.java
+++ dcm4che-net/src/main/java/org/dcm4che3/net/ApplicationEntity.java
@@ -400,13 +400,10 @@ public class ApplicationEntity implements Serializable {
     }
 
     public void addConnection(Connection conn) {
-        if (conn.getProtocol() == Connection.Protocol.DICOM || conn.getProtocol() == Connection.Protocol.HL7
-                || conn.getProtocol() == Connection.Protocol.SYSLOG_TLS || conn.getProtocol() == Connection.Protocol.SYSLOG_UDP) {
-            LOG.debug("New connection added has protocol DICOM or HL7 or SYSLOG_TLS or SYSLOG_UDP");
-        } else {
+        if (conn.getProtocol() == Connection.Protocol.DICOM)
             throw new IllegalArgumentException(
-                    "protocol != DICOM or HL7 or SYSLOG_TLS or SYSLOG_UDP - " + conn.getProtocol());
-        }
+                    "protocol != DICOM - " + conn.getProtocol());
+
 
         if (device != null && device != conn.getDevice())
             throw new IllegalStateException(conn + " not contained by Device: " + 

8f7168f152400a1c41f5979d1ccce365fcd10d6a
Gunter Zeilinger <gunterze@gmail.com>
2016-04-30 12:05:08 +0200

    dcm4che/dcm4chee-arc-light#50: WADO-RS

--- dcm4che-imageio/src/main/java/org/dcm4che3/imageio/stream/EncapsulatedPixelDataImageInputStream.java
+++ dcm4che-imageio/src/main/java/org/dcm4che3/imageio/stream/EncapsulatedPixelDataImageInputStream.java
@@ -137,4 +137,8 @@ public class EncapsulatedPixelDataImageInputStream extends MemoryCacheImageInput
         return true;
     }
 
+    public boolean isEndOfStream() {
+        return endOfStream;
+    }
+
 }

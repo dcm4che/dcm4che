158bc62950374f7bcf60cb29c25262c4a319c47e
rawmahn <roman.khazankin@gmail.com>
2016-06-13 14:52:30 +0200

    fix: [config, artf229983] add space as a valid character in a pathitem

--- dcm4che-conf/dcm4che-conf-core/src/test/java/org/dcm4che3/conf/core/XTest.java
+++ dcm4che-conf/dcm4che-conf-core/src/test/java/org/dcm4che3/conf/core/XTest.java
@@ -1,9 +1,12 @@
 package org.dcm4che3.conf.core;
 
+import org.dcm4che3.conf.core.util.SplittedPath;
 import org.dcm4che3.conf.core.util.XNodeUtil;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * Created by aprvf on 22.02.2016.
@@ -15,7 +18,25 @@ public class XTest {
 
         XNodeUtil.parseReference("/dicomConfigurationRoot/dicomDevicesRoot/dcm4chee_arc");
         XNodeUtil.parseReference("/dicomConfigurationRoot/dicomDevicesRoot/dcm4chee-arc");
+        XNodeUtil.parseReference("/dicomConfigurationRoot/dicomDevicesRoot/dcm4chee-arc");
+        XNodeUtil.parseReference("/dicomConfigurationRoot/dicomDevicesRoot/dcm4chee-arc/dicomNetworkAE/8.1_8.0.1");
 
         Assert.assertEquals(null, Nodes.simpleOrPersistablePathToPathItemsOrNull("//*[_.uuid='fd2c0253-ed6b-42a3-8d6a-3cef5c76cf34']"));
+    }
+
+    @Test
+    public void testSplittedPath() {
+
+        SplittedPath splittedPath = getSplittedPath("/dicomConfigurationRoot/dicomDevicesRoot/EXSYS1624/dicomNetworkAE/SCALLINONE ES");
+
+        Assert.assertNotNull(splittedPath);
+
+    }
+
+    private SplittedPath getSplittedPath(String path) {
+
+        List<String> pathItems = Nodes.simpleOrPersistablePathToPathItemsOrNull(path);
+        if (pathItems == null) return null;
+        return new SplittedPath(pathItems, 3);
     }
 }

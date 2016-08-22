ab250f01ac183ce8eb99c8ce589da2a12855acb3
rawmahn <roman.khazankin@gmail.com>
2016-08-22 16:51:32 +0200

    fix: [config] enable uuid indexing by default

--- dcm4che-conf/dcm4che-conf-dicom/src/main/java/org/dcm4che3/conf/dicom/DicomConfigurationBuilder.java
+++ dcm4che-conf/dcm4che-conf-dicom/src/main/java/org/dcm4che3/conf/dicom/DicomConfigurationBuilder.java
@@ -73,7 +73,7 @@ public class DicomConfigurationBuilder {
     private Configuration configurationStorage = null;
     private Map<Class, List<Class>> extensionClassesMap = new HashMap<Class, List<Class>>();
     private boolean doOptimisticLocking = false;
-    private boolean uuidIndexing = false;
+    private boolean uuidIndexing = true;
 
     private void setLdapProps(Hashtable<?, ?> ldapProps) {
         this.ldapProps = ldapProps;

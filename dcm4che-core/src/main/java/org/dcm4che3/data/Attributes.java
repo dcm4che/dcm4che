7af9b66386f0ec7533a2d1f899e4c0305ab3a927
Gunter Zeilinger <gunterze@gmail.com>
2016-08-28 13:37:44 +0200

    Fix dcm4che/dcm4chee-arc-light#341: Failed to store objects with Bulkdata in (0400,0561) Original Attributes Sequence

--- dcm4che-core/src/main/java/org/dcm4che3/data/Attributes.java
+++ dcm4che-core/src/main/java/org/dcm4che3/data/Attributes.java
@@ -3032,4 +3032,25 @@ public class Attributes implements Serializable {
         }
     }
 
+    public int removeAllBulkData() {
+        int removed = 0;
+        for (int i = 0; i < size; i++) {
+            Object value = values[i];
+            if (value instanceof BulkData) {
+                int srcPos = i + 1;
+                int len = size - srcPos;
+                System.arraycopy(tags, srcPos, tags, i, len);
+                System.arraycopy(vrs, srcPos, vrs, i, len);
+                System.arraycopy(values, srcPos, values, i, len);
+                i--;
+                size--;
+                removed++;
+            } else if (value instanceof Sequence) {
+                for (Attributes item : (Sequence) value) {
+                    removed += item.removeAllBulkData();
+                }
+            }
+        }
+        return removed;
+    }
 }

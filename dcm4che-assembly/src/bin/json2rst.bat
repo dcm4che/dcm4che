d01d59735b01c8950d9161c6806c434a20fe2d3d
Gunter Zeilinger <gunterze@gmail.com>
2016-09-15 15:41:30 +0200

    Fix dcm4che/dcm4chee-arc-light#375: Provide CL utility json2rst to generate Restructered Text from JSON schema files

---
+++ dcm4che-assembly/src/bin/json2rst.bat
@@ -0,0 +1,46 @@
+@echo off
+rem -------------------------------------------------------------------------
+rem json2rst  Launcher
+rem -------------------------------------------------------------------------
+
+if not "%ECHO%" == ""  echo %ECHO%
+if "%OS%" == "Windows_NT"  setlocal
+
+set MAIN_CLASS=org.dcm4che3.tool.json2rst.Json2Rst
+set MAIN_JAR=dcm4che-tool-json2rst-${project.version}.jar
+
+set DIRNAME=.\
+if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%
+
+rem Read all command line arguments
+
+set ARGS=
+:loop
+if [%1] == [] goto end
+        set ARGS=%ARGS% %1
+        shift
+        goto loop
+:end
+
+if not "%DCM4CHE_HOME%" == "" goto HAVE_DCM4CHE_HOME
+
+set DCM4CHE_HOME=%DIRNAME%..
+
+:HAVE_DCM4CHE_HOME
+
+if not "%JAVA_HOME%" == "" goto HAVE_JAVA_HOME
+
+set JAVA=java
+
+goto SKIP_SET_JAVA_HOME
+
+:HAVE_JAVA_HOME
+
+set JAVA=%JAVA_HOME%\bin\java
+
+:SKIP_SET_JAVA_HOME
+
+set CP=%DCM4CHE_HOME%\lib\%MAIN_JAR%
+set CP=%CP%;%DCM4CHE_HOME%\lib\javax.json-1.0.4.jar
+
+"%JAVA%" %JAVA_OPTS% -cp "%CP%" %MAIN_CLASS% %ARGS%

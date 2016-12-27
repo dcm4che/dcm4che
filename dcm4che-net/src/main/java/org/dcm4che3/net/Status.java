23ab117a071a2f873e5f101c83d3baf49280db6f
vrindanayak <vrinda.nayak@j4care.com>
2016-12-27 12:35:34 +0100

    Fix #498 dcm4che/dcm4chee-arc-light : Restrict C-MOVE Destinations to a configurable list of AE Titles for each Archive AE

--- dcm4che-net/src/main/java/org/dcm4che3/net/Status.java
+++ dcm4che-net/src/main/java/org/dcm4che3/net/Status.java
@@ -277,6 +277,7 @@ public class Status {
 
     public static final int UnableToProcess                    = 0xC000;
     public static final int CannotUnderstand                   = 0xC000;
+    public static final int MoveDestinationNotAllowed          = 0xC801;
 
     public static boolean isPending(int status) {
         return (status & Pending) == Pending;

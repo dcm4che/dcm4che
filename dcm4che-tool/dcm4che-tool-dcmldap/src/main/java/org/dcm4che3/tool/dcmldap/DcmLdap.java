2a1157dde9d778a4eef2c32284a4d855d060c03d
Vrinda Nayak <vrinda.nayak@j4care.com>
2017-06-28 14:29:01 +0200

    dcm4che/dcm4chee-arc-light#819 : dcm4che-tool-dcmldap: Utility to insert/remove configuration entries for Network AEs into/from LDAP server

--- dcm4che-tool/dcm4che-tool-dcmldap/src/main/java/org/dcm4che3/tool/dcmldap/DcmLdap.java
+++ dcm4che-tool/dcm4che-tool-dcmldap/src/main/java/org/dcm4che3/tool/dcmldap/DcmLdap.java
@@ -38,30 +38,163 @@
 
 package org.dcm4che3.tool.dcmldap;
 
-import org.apache.commons.cli.*;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.ParseException;
+
+import org.dcm4che3.conf.api.ConfigurationException;
+import org.dcm4che3.conf.api.DicomConfiguration;
+import org.dcm4che3.conf.ldap.LdapDicomConfiguration;
+import org.dcm4che3.net.ApplicationEntity;
+import org.dcm4che3.net.Connection;
+import org.dcm4che3.net.Device;
 import org.dcm4che3.tool.common.CLIUtils;
 
+import javax.naming.Context;
+import javax.naming.NamingException;
+import java.util.EnumSet;
+import java.util.Hashtable;
 import java.util.ResourceBundle;
 
 /**
  * @author Gunter Zeilinger <gunterze@gmail.com>
+ * @author Vrinda Nayak <vrinda.nayak@j4care.com>
  * @since Jun 2017
  */
 public class DcmLdap {
 
     private static ResourceBundle rb = ResourceBundle.getBundle("org.dcm4che3.tool.dcmldap.messages");
 
+    private static LdapDicomConfiguration config;
+
+    private static final String LDAP_URI = "ldap://localhost:389/dc=dcm4che,dc=org";
+    private static final String BIND_DN = "cn=admin,dc=dcm4che,dc=org";
+    private static final String PASSWORD = "secret";
+
     @SuppressWarnings("unchecked")
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try {
             CommandLine cl = parseComandLine(args);
-            DcmLdap main = new DcmLdap();
-            //TODO
+            setupLDAP(cl);
+            if (cl.hasOption("a"))
+                addNetworkAE(cl);
+            if (cl.hasOption("c"))
+                createNetworkAE(cl);
+            if (cl.hasOption("d"))
+                removeNetworkAE(cl);
         } catch (ParseException e) {
             System.err.println("dcmldap: " + e.getMessage());
             System.err.println(rb.getString("try"));
             System.exit(2);
         }
+    }
+
+    private static void setupLDAP(CommandLine cl) throws ConfigurationException {
+        Hashtable<String, Object> env = new Hashtable<>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put("java.naming.ldap.attributes.binary", "dicomVendorData");
+        env.put(Context.PROVIDER_URL, cl.hasOption("H") ? cl.getOptionValue("H") : LDAP_URI);
+        env.put(Context.SECURITY_PRINCIPAL, cl.hasOption("D") ? cl.getOptionValue("D") : BIND_DN);
+        env.put(Context.SECURITY_CREDENTIALS, cl.hasOption("w") ? cl.getOptionValue("w") : PASSWORD);
+        config = new LdapDicomConfiguration(env);
+    }
+
+    private static void addNetworkAE(CommandLine cl) throws ConfigurationException, ParseException {
+        Device device = config.findDevice(cl.getOptionValue("dev"));
+        createAE(cl, device);
+        config.merge(device, options(true));
+    }
+
+    private static void createNetworkAE(CommandLine cl) throws ConfigurationException, ParseException {
+        Device device = new Device();
+        createAE(cl, device);
+        config.persist(device, options(true));
+    }
+
+    private static void removeNetworkAE(CommandLine cl) throws ConfigurationException, NamingException {
+        String aet = cl.getOptionValue("d");
+        ApplicationEntity ae = config.findApplicationEntity(aet);
+        Device dev = ae.getDevice();
+        dev.removeApplicationEntity(ae);
+        dev.removeConnection(ae.getConnections().get(0));
+        config.merge(dev, options(true));
+        if (dev.getApplicationAETitles().isEmpty())
+            config.removeDevice(dev.getDeviceName(), null);
+    }
+
+    private static void createAE(CommandLine cl, Device device) throws ParseException {
+        String deviceName = device.getDeviceName();
+        String optVal = deviceName == null ? cl.getOptionValue("c") : cl.getOptionValue("a");
+
+        String[] aeHostPort = split(optVal , '@');
+        String[] hostPort = split(aeHostPort[1], ':');
+
+        if (deviceName == null) {
+            String name = cl.hasOption("dev") ? cl.getOptionValue("dev") : aeHostPort[0].toLowerCase();
+            device.setDeviceName(name);
+            if (cl.hasOption("dev-desc"))
+                device.setDescription(cl.getOptionValue("dev-desc"));
+            if (cl.hasOption("dev-type"))
+                device.setPrimaryDeviceTypes(cl.getOptionValue("dev-type"));
+        }
+
+        Connection conn = createConn(hostPort, cl);
+        device.addConnection(conn);
+
+        ApplicationEntity ae = new ApplicationEntity(aeHostPort[0]);
+        ae.addConnection(conn);
+        if (cl.hasOption("ae-desc"))
+            ae.setDescription(cl.getOptionValue("ae-desc"));
+        device.addApplicationEntity(ae);
+    }
+
+    private static Connection createConn(String[] hostPort, CommandLine cl) {
+        Connection conn = new Connection();
+        conn.setHostname(hostPort[0]);
+        conn.setPort(Integer.valueOf(hostPort[1]));
+        configureTLS(cl, conn);
+        String cn = cl.hasOption("conn")
+                        ? cl.getOptionValue("conn")
+                        : conn.getTlsCipherSuites().length > 1
+                            ? "dicom-tls"
+                            : "dicom";
+        conn.setCommonName(cn);
+        return conn;
+    }
+
+    private static void configureTLS(CommandLine cl, Connection conn) {
+        if (cl.hasOption("tls"))
+            conn.setTlsCipherSuites(
+                    "SSL_RSA_WITH_NULL_SHA",
+                    "TLS_RSA_WITH_AES_128_CBC_SHA",
+                    "SSL_RSA_WITH_3DES_EDE_CBC_SHA");
+        else if (cl.hasOption("tls-null"))
+            conn.setTlsCipherSuites("SSL_RSA_WITH_NULL_SHA");
+        else if (cl.hasOption("tls-3des"))
+            conn.setTlsCipherSuites("SSL_RSA_WITH_3DES_EDE_CBC_SHA");
+        else if (cl.hasOption("tls-aes"))
+            conn.setTlsCipherSuites(
+                    "TLS_RSA_WITH_AES_128_CBC_SHA",
+                    "SSL_RSA_WITH_3DES_EDE_CBC_SHA");
+        else if (cl.hasOption("tls-cipher"))
+            conn.setTlsCipherSuites(cl.getOptionValues("tls-cipher"));
+    }
+
+    private static String[] split(String s, char delim) throws ParseException {
+        int defPos = 0;
+        String[] s2 = new String[2];
+        int pos = s.indexOf(delim);
+        if (pos != -1) {
+            s2[0] = s.substring(0, pos);
+            s2[1] = s.substring(pos + 1);
+        } else {
+            s2[defPos] = s;
+        }
+        if (s2[1] == null)
+            throw new ParseException("Read how to specify <aet@host:port> in dcmldap help.");
+        return s2;
     }
 
     @SuppressWarnings("static-access")
@@ -150,4 +283,12 @@ public class DcmLdap {
         return cl;
     }
 
+    private static EnumSet<DicomConfiguration.Option> options(boolean register) {
+        EnumSet<DicomConfiguration.Option> options = EnumSet.of(
+                DicomConfiguration.Option.PRESERVE_VENDOR_DATA,
+                DicomConfiguration.Option.PRESERVE_CERTIFICATE);
+        if (register)
+            options.add(DicomConfiguration.Option.REGISTER);
+        return options;
+    }
 }

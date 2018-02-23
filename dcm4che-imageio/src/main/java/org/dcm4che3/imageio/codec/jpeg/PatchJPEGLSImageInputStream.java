f416bfdb0b4c9a5991f4b616a5499bab761bba77
Gunter Zeilinger <gunterze@gmail.com>
2018-02-23 14:16:37 +0100

    Add method to read all remaining bytes from PatchJPEGLSImageInputStream to OutputStream fix #237

--- dcm4che-imageio/src/main/java/org/dcm4che3/imageio/codec/jpeg/PatchJPEGLSImageInputStream.java
+++ dcm4che-imageio/src/main/java/org/dcm4che3/imageio/codec/jpeg/PatchJPEGLSImageInputStream.java
@@ -39,7 +39,9 @@
 package org.dcm4che3.imageio.codec.jpeg;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Arrays;
+import java.util.Objects;
 
 import javax.imageio.stream.ImageInputStream;
 import javax.imageio.stream.ImageInputStreamImpl;
@@ -58,6 +60,7 @@ public class PatchJPEGLSImageInputStream extends ImageInputStreamImpl {
     private static final Logger LOG =
             LoggerFactory.getLogger(PatchJPEGLSImageInputStream.class);
 
+    private static final int DEFAULT_BUFFER_SIZE = 8192;
     private final ImageInputStream iis;
     private long patchPos;
     private byte[] patch;
@@ -201,4 +204,24 @@ public class PatchJPEGLSImageInputStream extends ImageInputStreamImpl {
         iis.seek(adjustStreamPosition(pos));
     }
 
+    /**
+     * Reads all bytes from this image input stream and writes the bytes to the given output stream. This method
+     * does not close either stream.
+     *
+     * @param out the output stream, non-null
+     * @return the number of bytes transferred
+     * @throws IOException if an I/O error occurs when reading or writing
+     * @throws NullPointerException if out is {@code null}
+     */
+    public long transferTo(OutputStream out) throws IOException {
+        Objects.requireNonNull(out, "out");
+        long transferred = 0;
+        byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+        int read;
+        while ((read = this.read(buffer, 0, DEFAULT_BUFFER_SIZE)) > 0) {
+            out.write(buffer, 0, read);
+            transferred += read;
+        }
+        return transferred;
+    }
 }

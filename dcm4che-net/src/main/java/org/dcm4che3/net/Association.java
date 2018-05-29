1b21da89a5b102beed074e0a52c9ebc4bf49ca79
Salem Jamaluddeen <jd.salem@yahoo.com>
2018-05-29 11:27:32 -0400

    - Merging commits "bb239160ea2f2e2750c57bdbd271029aa644df9a" with desc"SocketException on association release fix #244" from master to generic-config.

--- dcm4che-net/src/main/java/org/dcm4che3/net/Association.java
+++ dcm4che-net/src/main/java/org/dcm4che3/net/Association.java
@@ -333,7 +333,9 @@ public class Association {
         LOG.info("{} << A-RELEASE-RQ", name);
         enterState(State.Sta7);
         stopTimeout();
-        encoder.writeAReleaseRQ();
+        synchronized (this) {
+            encoder.writeAReleaseRQ();
+        }
         startReleaseTimeout();
     }
 

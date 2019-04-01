958999e0ea02c62cc2d15fc11ace340162fbd536
Sam Lewis <sam@focus21.io>
2019-04-01 14:27:43 -0400

    Added quotation marks around JAVA_LIBRARY_PATH in tools executables
    Without the quotation marks the command will fail to execute
    if the path contains spaces

--- dcm4che-assembly/src/bin/dcm2jpg.bat
+++ dcm4che-assembly/src/bin/dcm2jpg.bat
@@ -61,7 +61,7 @@ rem Setup the native library path
 "%JAVA%" -d64 -version >nul 2>&1 && set OS=win-x86_64 || set OS=win-i686
 set JAVA_LIBRARY_PATH=%DCM4CHE_HOME%\lib\%OS%
 
-set JAVA_OPTS=%JAVA_OPTS% -Djava.library.path=%JAVA_LIBRARY_PATH%
+set JAVA_OPTS=%JAVA_OPTS% -Djava.library.path="%JAVA_LIBRARY_PATH%"
 
 if not "%IMAGE_READER_FACTORY%" == "" ^
  set JAVA_OPTS=%JAVA_OPTS% -Dorg.dcm4che3.imageio.codec.ImageReaderFactory=%IMAGE_READER_FACTORY%

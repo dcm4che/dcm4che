39adc2ddb5af263bab3555f601c889b342332518
Gunter Zeilinger <gunterze@gmail.com>
2019-05-22 19:04:13 +0200

    add DicomInputStreamTest.testSpoolDataFragments()

--- dcm4che-core/src/test/java/org/dcm4che3/io/DicomInputStreamTest.java
+++ dcm4che-core/src/test/java/org/dcm4che3/io/DicomInputStreamTest.java
@@ -1,12 +1,14 @@
 package org.dcm4che3.io;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.Collections;
+import java.util.List;
 
-import org.dcm4che3.data.Tag;
-import org.dcm4che3.data.Attributes;
-import org.dcm4che3.data.Sequence;
+import org.dcm4che3.data.*;
 import org.dcm4che3.io.DicomInputStream.IncludeBulkData;
 import org.junit.Test;
 
@@ -39,6 +41,33 @@ public class DicomInputStreamTest {
     public void testImplicitVR() throws Exception {
         Attributes attrs = readFrom("OT-PAL-8-face", IncludeBulkData.URI);
         assertEquals(1, attrs.getInt(Tag.SamplesPerPixel, 0));
+    }
+
+    @Test
+    public void testSpoolDataFragments() throws Exception {
+        List<File> bulkDataFiles;
+        Object pixeldata;
+        try (DicomInputStream in = new DicomInputStream(new File("target/test-data/US-PAL-8-10x-echo"))) {
+            in.setIncludeBulkData(IncludeBulkData.URI);
+            in.setURI(null); // force spooling
+            pixeldata = in.readDataset(-1, -1).getValue(Tag.PixelData);
+            bulkDataFiles = in.getBulkDataFiles();
+        }
+        try {
+            assertTrue(pixeldata instanceof Fragments);
+            Fragments fragments = (Fragments) pixeldata;
+            assertEquals(11, fragments.size());
+            assertEquals(11, bulkDataFiles.size());
+            for (int i = 0; i < 10; i++) {
+                Object fragment = fragments.get(++i);
+                assertTrue(fragment instanceof BulkData);
+                assertEquals(bulkDataFiles.get(i), ((BulkData)fragment).getFile());
+            }
+        } finally {
+            for (File bulkDataFile : bulkDataFiles) {
+                bulkDataFile.delete();
+            }
+        }
     }
 
     private static Attributes readFrom(String name, IncludeBulkData includeBulkData) throws Exception {

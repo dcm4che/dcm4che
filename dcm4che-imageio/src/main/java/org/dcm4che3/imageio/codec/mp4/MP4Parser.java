ce0005d6589ac3deeaca940ab783cc9ee97de93a
Gunter Zeilinger <gunterze@gmail.com>
2019-06-24 11:58:06 +0200

    Support extracting Image Pixel Description Macro Attributes from MPEG-4 Part 14 files fix #239

---
+++ dcm4che-imageio/src/main/java/org/dcm4che3/imageio/codec/mp4/MP4Parser.java
@@ -0,0 +1,356 @@
+/*
+ * **** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * J4Care.
+ * Portions created by the Initial Developer are Copyright (C) 2015-2019
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * **** END LICENSE BLOCK *****
+ *
+ */
+
+package org.dcm4che3.imageio.codec.mp4;
+
+import org.dcm4che3.data.Attributes;
+import org.dcm4che3.data.Tag;
+import org.dcm4che3.data.UID;
+import org.dcm4che3.data.VR;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.util.Date;
+
+/**
+ * @author Gunter Zeilinger <gunterze@gmail.com>
+ * @since Jun 2019
+ */
+public class MP4Parser {
+    private static final int MovieBoxType = 0x6d6f6f76; // moov;
+    private static final int TrackBoxType = 0x7472616b; // trak
+    private static final int MediaBoxType = 0x6d646961; // mdia
+    private static final int MediaHeaderBoxType = 0x6d646864; // mdhd
+    private static final int MediaInformationBoxType = 0x6d696e66; // minf
+    private static final int SampleTableBoxType = 0x7374626c; // stbl
+    private static final int SampleDescriptionBoxType = 0x73747364; // stsd
+    private static final int VisualSampleEntryTypeAVC1 = 0x61766331; // avc1
+    private static final int AvcConfigurationBoxType = 0x61766343; // avcC
+    private static final int VisualSampleEntryTypeHVC1 = 0x68766331; // hvc1
+    private static final int HevcConfigurationBoxType = 0x68766343; // hvcC
+    private static final int SampleSizeBoxType = 0x7374737a; // stsz
+
+    private ByteBuffer buf = ByteBuffer.allocate(8);
+    private Date creationTime;
+    private Date modificationTime;
+    private int timescale;
+    private long duration;
+    private int fp1000s;
+    private int rows;
+    private int columns;
+    private int numFrames;
+    private int visualSampleEntryType;
+    private int configurationVersion;
+    private int profile_idc;
+    private int level_idc;
+
+    public MP4Parser(SeekableByteChannel channel) throws IOException {
+        parseMovieBox(channel, findBox(channel, channel.size() - channel.position(), MovieBoxType));
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public Date getModificationTime() {
+        return modificationTime;
+    }
+
+    public Attributes getAttributes(Attributes attrs) {
+        if (attrs == null)
+            attrs = new Attributes(14);
+
+        attrs.setInt(Tag.CineRate, VR.IS, (fp1000s + 500) / 1000);
+        attrs.setFloat(Tag.FrameTime, VR.DS, 1_000_000.f / fp1000s);
+        attrs.setInt(Tag.SamplesPerPixel, VR.US, 3);
+        attrs.setString(Tag.PhotometricInterpretation, VR.CS, "YBR_PARTIAL_420");
+        attrs.setInt(Tag.PlanarConfiguration, VR.US, 0);
+        attrs.setInt(Tag.FrameIncrementPointer, VR.AT, Tag.FrameTime);
+        attrs.setInt(Tag.NumberOfFrames, VR.IS, numFrames);
+        attrs.setInt(Tag.Rows, VR.US, rows);
+        attrs.setInt(Tag.Columns, VR.US, columns);
+        attrs.setInt(Tag.BitsAllocated, VR.US, 8);
+        attrs.setInt(Tag.BitsStored, VR.US, 8);
+        attrs.setInt(Tag.HighBit, VR.US, 7);
+        attrs.setInt(Tag.PixelRepresentation, VR.US, 0);
+        attrs.setString(Tag.LossyImageCompression, VR.CS, "01");
+        return attrs;
+    }
+
+    public String getTransferSyntaxUID() throws MP4ParserException {
+        switch (visualSampleEntryType) {
+            case VisualSampleEntryTypeAVC1:
+                switch (profile_idc) {
+                    case 100: // High Profile
+                        if (level_idc <= 41)
+                            return isBDCompatible()
+                                    ? UID.MPEG4AVCH264BDCompatibleHighProfileLevel41
+                                    : UID.MPEG4AVCH264HighProfileLevel41;
+                        else if (level_idc <= 42)
+                            // TODO: distinguish between MPEG4AVCH264HighProfileLevel42For2DVideo
+                            //  and MPEG4AVCH264HighProfileLevel42For3DVideo
+                            return UID.MPEG4AVCH264HighProfileLevel42For2DVideo;
+                        break;
+                    case 128: // Stereo High Profile
+                        if (level_idc <= 42)
+                            return UID.MPEG4AVCH264StereoHighProfileLevel42;
+                        break;
+                }
+                throw avcProfileLevelNotSupported();
+            case VisualSampleEntryTypeHVC1:
+                if (level_idc <= 51) {
+                    switch (profile_idc) {
+                        case 1: // Main Profile
+                            return UID.HEVCH265MainProfileLevel51;
+                        case 2: // Main 10 Profile
+                            return UID.HEVCH265Main10ProfileLevel51;
+                    }
+                }
+                throw hevcProfileLevelNotSupported();
+        }
+        throw new AssertionError("visualSampleEntryType:" + visualSampleEntryType);
+    }
+
+    private MP4ParserException avcProfileLevelNotSupported() {
+        return new MP4ParserException("AVC profile_idc/level_idc: " + profile_idc + '/' + level_idc + " not supported");
+    }
+
+    private MP4ParserException hevcProfileLevelNotSupported() {
+        return new MP4ParserException("HEVC profile_idc/level_idc: " + profile_idc + '/' + level_idc + " not supported");
+    }
+
+    private boolean isBDCompatible() {
+        return rows == 1080
+                ? columns == 1920
+                    && (fp1000s == 23976 || fp1000s == 24000 || fp1000s == 25000 || fp1000s == 29970)
+                : rows == 720 && columns == 1280
+                    && (fp1000s == 23976 || fp1000s == 24000 || fp1000s == 50000 || fp1000s == 59940);
+    }
+
+    private Box nextBox(SeekableByteChannel channel, long remaining) throws IOException {
+        int size = readInt(channel);
+        int type = readInt(channel);
+        return size == 0
+                ? new Box(type, remaining - 8)
+                : size == 1
+                ? new Box(type, readLong(channel) - 16)
+                : new Box(type, size - 8);
+    }
+
+    private Box findBox(SeekableByteChannel channel, long end, int... types)
+            throws IOException {
+        long remaining;
+        while ((remaining = end - channel.position()) > 0) {
+            Box box = nextBox(channel, remaining);
+            for (int type : types) {
+                if (box.type == type)
+                    return box;
+            }
+            skip(channel, box.contentSize);
+        }
+        throw new MP4ParserException(boxNotFound(types[0]));
+    }
+
+    private static String boxNotFound(int type) {
+        return String.format("%c%c%c%c box not found",
+                (type >> 24) & 0xff,
+                (type >> 16) & 0xff,
+                (type >> 8) & 0xff,
+                type & 0xff);
+    }
+
+    private int readInt(SeekableByteChannel channel) throws IOException {
+        buf.clear().limit(4);
+        channel.read(buf);
+        buf.rewind();
+        return buf.getInt();
+    }
+
+    private long readLong(SeekableByteChannel channel) throws IOException {
+        buf.clear();
+        channel.read(buf);
+        buf.rewind();
+        return buf.getLong();
+    }
+
+    private void skip(SeekableByteChannel channel, long n) throws IOException {
+        channel.position(channel.position() + n);
+    }
+
+    private static Date toDate(long val) {
+        return val > 0 ? new Date((val - 2082844800L) * 1000L) : null;
+    }
+
+    private static class Box {
+        final int type;
+        final long contentSize;
+
+        Box(int type, long contentSize) {
+            this.type = type;
+            this.contentSize = contentSize;
+        }
+    }
+
+    private void parseMovieBox(SeekableByteChannel channel, Box box) throws IOException {
+        long end = channel.position() + box.contentSize;
+        parseTrackBox(channel, findBox(channel, end, TrackBoxType));
+        channel.position(end);
+    }
+
+    private void parseTrackBox(SeekableByteChannel channel, Box box) throws IOException {
+        long end = channel.position() + box.contentSize;
+        parseMediaBox(channel, findBox(channel, end, MediaBoxType));
+        channel.position(end);
+    }
+
+    private void parseMediaBox(SeekableByteChannel channel, Box box) throws IOException {
+        long end = channel.position() + box.contentSize;
+        parseMediaHeaderBox(channel, findBox(channel, end, MediaHeaderBoxType));
+        parseMediaInformationBox(channel, findBox(channel, end, MediaInformationBoxType));
+        channel.position(end);
+    }
+
+    private void parseMediaHeaderBox(SeekableByteChannel channel, Box box) throws IOException {
+        long end = channel.position() + box.contentSize;
+        buf.clear().limit(4);
+        channel.read(buf);
+        buf.rewind();
+        if (buf.get() == 1) {
+            creationTime = toDate(readLong(channel));
+            modificationTime = toDate(readLong(channel));
+            timescale = readInt(channel);
+            duration = readLong(channel);
+        } else {
+            creationTime = toDate(readInt(channel) & 0xffffffffL);
+            modificationTime = toDate(readInt(channel) & 0xffffffffL);
+            timescale = readInt(channel);
+            duration = readInt(channel) & 0xffffffffL;
+        }
+        channel.position(end);
+    }
+
+    private void parseMediaInformationBox(SeekableByteChannel channel, Box box) throws IOException {
+        long end = channel.position() + box.contentSize;
+        parseSampleTableBox(channel,
+                findBox(channel, end, SampleTableBoxType));
+        channel.position(end);
+    }
+
+    private void parseSampleTableBox(SeekableByteChannel channel, Box box) throws IOException {
+        long end = channel.position() + box.contentSize;
+        parseSampleDescriptionBox(channel,
+                findBox(channel, end, SampleDescriptionBoxType));
+        parseSampleSizeBox(channel,
+                findBox(channel, end, SampleSizeBoxType));
+        channel.position(end);
+    }
+
+    private void parseSampleDescriptionBox(SeekableByteChannel channel, Box box) throws IOException {
+        long end = channel.position() + box.contentSize;
+        skip(channel, 8);
+        parseVisualSampleEntry(channel,
+                findBox(channel, end, VisualSampleEntryTypeAVC1, VisualSampleEntryTypeHVC1));
+        channel.position(end);
+    }
+
+    private void parseVisualSampleEntry(SeekableByteChannel channel, Box box) throws IOException {
+        long end = channel.position() + box.contentSize;
+        visualSampleEntryType = box.type;
+        skip(channel, 24);
+        buf.clear().limit(4);
+        channel.read(buf);
+        buf.rewind();
+        columns = buf.getShort();
+        rows = buf.getShort();
+        skip(channel, 50);
+        switch (box.type) {
+            case VisualSampleEntryTypeAVC1:
+                parseAvcConfigurationBox(channel,
+                        findBox(channel, end, AvcConfigurationBoxType));
+                break;
+            case VisualSampleEntryTypeHVC1:
+                parseHevcConfigurationBox(channel,
+                        findBox(channel, end, HevcConfigurationBoxType));
+                break;
+        }
+        channel.position(end);
+    }
+
+    private void parseAvcConfigurationBox(SeekableByteChannel channel, Box box) throws IOException {
+        long end = channel.position() + box.contentSize;
+        buf.clear().limit(4);
+        channel.read(buf);
+        buf.rewind();
+        configurationVersion = buf.get() & 0xff;
+        profile_idc = buf.get() & 0xff;
+        buf.get();
+        level_idc = buf.get() & 0xff;
+        channel.position(end);
+    }
+
+    private void parseHevcConfigurationBox(SeekableByteChannel channel, Box box) throws IOException {
+        long end = channel.position() + box.contentSize;
+        buf.clear().limit(2);
+        channel.read(buf);
+        buf.rewind();
+        configurationVersion = buf.get() & 0xff;
+        profile_idc = (buf.get() & 0x1F);
+        skip(channel, 10);
+        buf.clear().limit(1);
+        channel.read(buf);
+        buf.rewind();
+        level_idc = buf.get() & 0xff;
+        channel.position(end);
+    }
+
+    private void parseSampleSizeBox(SeekableByteChannel channel, Box box) throws IOException {
+        long end = channel.position() + box.contentSize;
+        skip(channel, 8);
+        buf.clear().limit(4);
+        channel.read(buf);
+        buf.rewind();
+        numFrames = buf.getInt();
+        fp1000s = (int) ((numFrames * 1000L * timescale + (duration >> 1)) / duration);
+        channel.position(end);
+    }
+
+}

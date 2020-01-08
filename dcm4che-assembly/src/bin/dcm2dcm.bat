f0e03e9096dff9111916590c481c9e78dcbbf1aa
Georg Habeck <geha00@gmail.com>
2020-01-08 18:21:39 +0100

    Detect 64-bit VM in tool launchers under Windows for Java 10+

--- dcm4che-assembly/src/bin/dcm2dcm.bat
+++ dcm4che-assembly/src/bin/dcm2dcm.bat
@@ -58,7 +58,7 @@ set CP=%CP%;%DCM4CHE_HOME%\lib\log4j-${log4j.version}.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\commons-cli-${commons-cli.version}.jar
 
 rem Setup the native library path
-"%JAVA%" -d64 -version >nul 2>&1 && set OS=win-x86_64 || set OS=win-i686
+"%JAVA%" -version 2>&1 | findstr 64-Bit >nul && set "OS=win-x86_64" || set "OS=win-i686"
 set "JAVA_LIBRARY_PATH=%DCM4CHE_HOME%\lib\%OS%"
 
 set JAVA_OPTS=%JAVA_OPTS% -Djava.library.path=%JAVA_LIBRARY_PATH%

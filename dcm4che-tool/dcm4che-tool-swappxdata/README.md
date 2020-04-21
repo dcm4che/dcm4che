cab977995ad085f371ccda918c8cf7cef75cf6ce
Gunter Zeilinger <gunterze@protonmail.com>
2020-04-21 14:43:02 +0200

    swappxdata: only swap bytes of big endian pixel data fix #708

---
+++ dcm4che-tool/dcm4che-tool-swappxdata/README.md
@@ -0,0 +1,27 @@
+```
+usage: swappxdata [--ifBigEndian [--testAll]] <dicom-file>|<directory>...
+
+The swappxdata utility swaps bytes of uncompressed pixel data with Value
+Representation OW.
+For each successfully updated file a dot (.) character is written to stdout.
+If an error occurs on updating a file, an E character is written to stdout
+and a stack trace is written to stderr.
+For each file kept untouched, one of the characters:
+p - no pixel data
+c - compressed pixel data
+b - pixel data with Value Representation OB
+l - little endian encoded pixel data
+8 - pixel data with 8 bits allocated
+is written to stdout.
+
+Options:
+--ifBigEndian    test encoding of pixel data; keep files untouched, if the
+                 pixel data is encoded with little endian or 8 bits allocated.
+                 By default, bytes of uncompressed pixel data with Value
+                 Representation OW will be swapped, independent of its
+                 encoding.
+--testAll        test encoding of pixel data of each file. By default, if one
+                 file of a directory is detected as not big endian encoded,
+                 all remaining files of the directory are kept also untouched
+                 without loading them in memory for testing.
+```
--- dcm4che-tool/dcm4che-tool-swappxdata/src/main/java/org/dcm4che3/tool/swappxdata/SwapPxData.java
+++ dcm4che-tool/dcm4che-tool-swappxdata/src/main/java/org/dcm4che3/tool/swappxdata/SwapPxData.java
@@ -50,7 +50,6 @@ import org.dcm4che3.util.ByteUtils;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.ArrayList;
 
 /**
  * @author Gunter Zeilinger <gunterze@gmail.com>
@@ -58,19 +57,59 @@ import java.util.ArrayList;
  */
 public class SwapPxData {
 
+    private static final String[] USAGE = {
+            "usage: swappxdata [--ifBigEndian [--testAll]] <dicom-file>|<directory>...",
+            "",
+            "The swappxdata utility swaps bytes of uncompressed pixel data with Value",
+            "Representation OW.",
+            "For each successfully updated file a dot (.) character is written to stdout.",
+            "If an error occurs on updating a file, an E character is written to stdout",
+            "and a stack trace is written to stderr.",
+            "For each file kept untouched, one of the characters:",
+            "p - no pixel data",
+            "c - compressed pixel data",
+            "b - pixel data with Value Representation OB",
+            "l - little endian encoded pixel data",
+            "8 - pixel data with 8 bits allocated",
+            "is written to stdout.",
+            "",
+            "Options:",
+            "--ifBigEndian    test encoding of pixel data; keep files untouched, if the",
+            "                 pixel data is encoded with little endian or 8 bits allocated.",
+            "                 By default, bytes of uncompressed pixel data with Value",
+            "                 Representation OW will be swapped, independent of its",
+            "                 encoding.",
+            "--testAll        test encoding of pixel data of each file. By default, if one",
+            "                 file of a directory is detected as not big endian encoded,",
+            "                 all remaining files of the directory are kept also untouched",
+            "                 without loading them in memory for testing." };
+    private final boolean ifBigEndian;
+    private final boolean testAll;
+    private String skipDir;
+    private char skipChar;
     private int updated;
     private int skipped;
     private int failed;
 
-    public static void main(String[] args) throws Exception {
-        if (args.length == 0) {
-            System.out.println("Usage: swappxdata <dicom-file>|<directory>...");
+    public SwapPxData(boolean ifBigEndian, boolean testAll) {
+        this.ifBigEndian = ifBigEndian;
+        this.testAll = testAll;
+    }
+
+    public static void main(String[] args) {
+        boolean ifBigEndian = args.length > 0 && args[0].equals("--ifBigEndian");
+        boolean testAll = ifBigEndian && args.length > 1 && args[1].equals("--testAll");
+        int firstPath = ifBigEndian ? testAll ? 2 : 1 : 0;
+        if (args.length == firstPath || args[firstPath].startsWith("-")) {
+            for (String line : USAGE) {
+                System.out.println(line);
+            }
             System.exit(-1);
         }
+        SwapPxData inst = new SwapPxData(ifBigEndian, testAll);
         long start = System.currentTimeMillis();
-        SwapPxData inst = new SwapPxData();
-        for (String path : args) {
-            inst.processFileOrDirectory(new File(path));
+        for (int i = firstPath; i < args.length; i++) {
+            inst.processFileOrDirectory(new File(args[i]));
         }
         long stop = System.currentTimeMillis();
         System.out.println();
@@ -95,6 +134,10 @@ public class SwapPxData {
     }
 
     private char processFile(File file) {
+        if (skipDir != null && skipDir.equals(file.getParent())) {
+            skipped++;
+            return skipChar;
+        }
         try {
             Attributes dataset;
             try (DicomInputStream is = new DicomInputStream(file)) {
@@ -104,8 +147,19 @@ public class SwapPxData {
             VR.Holder vr = new VR.Holder();
             Object value = dataset.getValue(Tag.PixelData, vr);
             if ((value instanceof BulkData) && vr.vr == VR.OW) {
+                if (ifBigEndian && dataset.getInt(Tag.BitsAllocated, 8) != 16) {
+                    skipped++;
+                    if (!testAll)
+                        skipDir = file.getParent();
+                    return skipChar = '8';
+                }
                 try (RandomAccessFile raf = new RandomAccessFile(file, "rw")) {
-                    toggleEndian(raf, (BulkData) value);
+                    if (!toggleEndian(raf, (BulkData) value)) {
+                        skipped++;
+                        if (!testAll)
+                            skipDir = file.getParent();
+                        return skipChar = 'l';
+                    }
                 }
                 updated++;
                 return '.';
@@ -120,12 +174,24 @@ public class SwapPxData {
         return 'E';
     }
 
-    private void toggleEndian(RandomAccessFile raf, BulkData bulkData) throws IOException {
+    private boolean toggleEndian(RandomAccessFile raf, BulkData bulkData) throws IOException {
         byte[] b = new byte[bulkData.length()];
         raf.seek(bulkData.offset());
         raf.readFully(b);
+        if (ifBigEndian) {
+            int prev = ByteUtils.bytesToShortBE(b, 0);
+            long sumDiffs = 0L;
+            for (int off = 2, end = b.length - 1; off < end; off++, off++) {
+                int val = ByteUtils.bytesToShortBE(b, off);
+                sumDiffs += Math.abs(val - prev);
+                prev = val;
+            }
+            if (sumDiffs > (((long) b.length) << 7))
+                return false;
+        }
         raf.seek(bulkData.offset());
         raf.write(ByteUtils.swapShorts(b, 0, b.length));
+        return true;
     }
 
 }

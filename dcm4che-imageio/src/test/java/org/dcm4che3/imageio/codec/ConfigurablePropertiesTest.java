dbd92b3399581ee8df2a063e75c4d25bbdba1582
Zoe.Zhang <EMWDN@agfahealthcare.com>
2020-05-05 15:23:05 -0400

    Remove headers

--- dcm4che-imageio/src/test/java/org/dcm4che3/imageio/codec/ConfigurablePropertiesTest.java
+++ dcm4che-imageio/src/test/java/org/dcm4che3/imageio/codec/ConfigurablePropertiesTest.java
@@ -1,18 +1,3 @@
-//
-// ///////////////////////////////////////////////////////////////
-//                 C O P Y R I G H T  (c) 2020                 //
-//         Agfa HealthCare N.V. and/or its affiliates          //
-//                    All Rights Reserved                      //
-/////////////////////////////////////////////////////////////////
-//                                                             //
-//        THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF       //
-//         Agfa HealthCare N.V. and/or its affiliates.         //
-//       The copyright notice above does not evidence any      //
-//      actual or intended publication of such source code.    //
-//                                                             //
-/////////////////////////////////////////////////////////////////
-//
-
 package org.dcm4che3.imageio.codec;
 
 import org.dcm4che3.conf.test.ConfigurablePropertiesTestBase;

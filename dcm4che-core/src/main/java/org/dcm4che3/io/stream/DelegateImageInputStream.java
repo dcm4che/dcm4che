c9f2afe659d267ecd3e393e5ddc718ea2c62277c
Zoe.Zhang <EMWDN@agfahealthcare.com>
2020-05-05 15:23:05 -0400

    Remove headers

--- dcm4che-core/src/main/java/org/dcm4che3/io/stream/DelegateImageInputStream.java
+++ dcm4che-core/src/main/java/org/dcm4che3/io/stream/DelegateImageInputStream.java
@@ -1,17 +1,3 @@
-//
-///////////////////////////////////////////////////////////////
-//                C O P Y R I G H T  (c) 2019                //
-//        Agfa HealthCare N.V. and/or its affiliates         //
-//                    All Rights Reserved                    //
-///////////////////////////////////////////////////////////////
-//                                                           //
-//       THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF      //
-//        Agfa HealthCare N.V. and/or its affiliates.        //
-//      The copyright notice above does not evidence any     //
-//     actual or intended publication of such source code.   //
-//                                                           //
-///////////////////////////////////////////////////////////////
-//
 package org.dcm4che3.io.stream;
 
 import javax.imageio.stream.IIOByteBuffer;
--- dcm4che-imageio/src/main/java/org/dcm4che3/imageio/plugins/dcm/DicomImageAccessorMetaData.java
+++ dcm4che-imageio/src/main/java/org/dcm4che3/imageio/plugins/dcm/DicomImageAccessorMetaData.java
@@ -1,17 +1,3 @@
-//
-///////////////////////////////////////////////////////////////
-//                C O P Y R I G H T  (c) 2019                //
-//        Agfa HealthCare N.V. and/or its affiliates         //
-//                    All Rights Reserved                    //
-///////////////////////////////////////////////////////////////
-//                                                           //
-//       THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF      //
-//        Agfa HealthCare N.V. and/or its affiliates.        //
-//      The copyright notice above does not evidence any     //
-//     actual or intended publication of such source code.   //
-//                                                           //
-///////////////////////////////////////////////////////////////
-//
 package org.dcm4che3.imageio.plugins.dcm;
 
 import org.dcm4che3.imageio.metadata.DicomImageAccessor;

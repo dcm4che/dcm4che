47937c2fdeaf198454ca73cc0e5d36ebeddb9c14
Vrinda <vrinda.nayak@j4care.com>
2020-11-12 17:22:48 +0100

    Fix #820 : mppsscu utility : User provided attributes of ScheduledStepAttributesSequence is not inserted in MPPS dataset

--- dcm4che-tool/dcm4che-tool-mppsscu/src/main/java/org/dcm4che3/tool/mppsscu/MppsSCU.java
+++ dcm4che-tool/dcm4che-tool-mppsscu/src/main/java/org/dcm4che3/tool/mppsscu/MppsSCU.java
@@ -650,10 +650,11 @@ public class MppsSCU {
             dcrSeq.add(new Attributes(discontinuationReason));
 
         Sequence raSeq = inst.getSequence(Tag.RequestAttributesSequence);
+        Sequence ssaSeq1 = inst.ensureSequence(Tag.ScheduledStepAttributesSequence, 1);
         if (raSeq == null || raSeq.isEmpty()) {
-            Sequence ssaSeq = 
+            Sequence ssaSeq =
                     mpps.newSequence(Tag.ScheduledStepAttributesSequence, 1);
-            Attributes ssa = new Attributes();
+            Attributes ssa = new Attributes(ssaSeq1.get(0));
             ssaSeq.add(ssa);
             for (int tag : SSA_TYPE_2_ATTRS)
                 ssa.setNull(tag, dict.vrOf(tag));
@@ -662,7 +663,7 @@ public class MppsSCU {
             Sequence ssaSeq =
                     mpps.newSequence(Tag.ScheduledStepAttributesSequence, raSeq.size());
             for (Attributes ra : raSeq) {
-                Attributes ssa = new Attributes();
+                Attributes ssa = new Attributes(ssaSeq1.get(0));
                 ssaSeq.add(ssa);
                 for (int tag : SSA_TYPE_2_ATTRS)
                     ssa.setNull(tag, dict.vrOf(tag));

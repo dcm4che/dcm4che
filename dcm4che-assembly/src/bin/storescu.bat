2be49acf387dcaa2109f4ace87697c4a7d57bb9a
Gunter Zeilinger <gunterze@protonmail.com>
2020-11-15 21:45:49 +0100

    Adjust directory structure with platform specific native libraries to OSGI OS and Processor Names in binary distribution package fix #824

--- dcm4che-assembly/src/bin/storescu.bat
+++ dcm4che-assembly/src/bin/storescu.bat
@@ -59,7 +59,7 @@ set CP=%CP%;%DCM4CHE_HOME%\lib\log4j-${log4j.version}.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\commons-cli-${commons-cli.version}.jar
 
 rem Setup native library path
-"%JAVA%" -version 2>&1 | findstr 64-Bit >nul && set "OS=win-x86_64" || set "OS=win-i686"
+"%JAVA%" -version 2>&1 | findstr 64-Bit >nul && set "OS=windows-x86-64" || set "OS=windows-x86"
 set JAVA_LIBRARY_PATH=%DCM4CHE_HOME%\lib\%OS%
 
 set JAVA_OPTS=%JAVA_OPTS% -Djava.library.path="%JAVA_LIBRARY_PATH%"

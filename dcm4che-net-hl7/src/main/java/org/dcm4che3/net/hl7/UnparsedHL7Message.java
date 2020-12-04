9f980a235c588874c959fb3863b0002efededc8d
Gunter Zeilinger <gunterze@protonmail.com>
2020-12-04 13:41:38 +0100

    Support unescape hexadecimal data encoded in \Xdddd\ escape sequences in HL7 Messages fix #845

--- dcm4che-net-hl7/src/main/java/org/dcm4che3/net/hl7/UnparsedHL7Message.java
+++ dcm4che-net-hl7/src/main/java/org/dcm4che3/net/hl7/UnparsedHL7Message.java
@@ -44,6 +44,7 @@ import org.dcm4che3.hl7.HL7Segment;
 
 import java.io.Serializable;
 import java.text.ParsePosition;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -54,8 +55,9 @@ public class UnparsedHL7Message implements Serializable {
     private static final AtomicInteger prevSerialNo = new AtomicInteger();
     private final int serialNo;
     private final byte[] data;
-    private transient HL7Segment msh;
-    private transient int mshLength;
+    private transient volatile byte[] unescapeXdddd;
+    private transient volatile HL7Segment msh;
+    private transient volatile int mshLength;
 
     public UnparsedHL7Message(byte[] data) {
         this.serialNo = prevSerialNo.incrementAndGet();
@@ -81,5 +83,110 @@ public class UnparsedHL7Message implements Serializable {
 
     public byte[] data() {
         return data;
+    }
+
+    /**
+     * Return HL7 message with unescaped hexdata from \Xdddd\ escape sequences.
+     * Does not unescape \Xdddd\ escape sequences which contains a field separator,
+     * component separator, subcomponent separator, repetition separator or escape character.
+     *
+     * @return HL7 message with unescaped hexdata from \Xdddd\ escape sequences
+     */
+    public byte[] unescapeXdddd() {
+        if (unescapeXdddd == null)
+            unescapeXdddd = unescapeXdddd(data);
+        return unescapeXdddd;
+    }
+
+    private static byte[] unescapeXdddd(byte[] data) {
+        int[] pos = findEscapeXdddd(data);
+        return pos.length == 0 ? data : replaceXdddd(data, pos);
+    }
+
+    private static byte[] replaceXdddd(byte[] src, int[] pos) {
+        byte[] dest = new byte[src.length - calcLengthDecrement(pos)];
+        int srcPos = 0;
+        int destPos = 0;
+        int i = 0;
+        do {
+            int length = pos[i] - srcPos - 2;
+            System.arraycopy(src, srcPos, dest, destPos, length);
+            srcPos += length;
+            length = replaceXdddd(src, pos[i], pos[++i], dest, destPos += length);
+            srcPos += 3 + length;
+            destPos += length / 2;
+        } while (++i < pos.length);
+        System.arraycopy(src, srcPos, dest, destPos, src.length - srcPos);
+        return dest;
+    }
+
+    private static int replaceXdddd(byte[] src, int beginIndex, int endIndex, byte[] dest, int destPos) {
+        for (int i = beginIndex; i < endIndex;) {
+            dest[destPos++] = (byte) ((parseHex(src[i++]) << 4) | parseHex(src[i++]));
+        }
+        return endIndex - beginIndex;
+    }
+
+    private static int calcLengthDecrement(int[] pos) {
+        int i = pos.length;
+        int l = 0;
+        do {
+            l += pos[--i];
+            l -= pos[--i];
+        } while (i > 0);
+        return (l + pos.length * 3) / 2;
+    }
+
+    private static int[] findEscapeXdddd(byte[] data) {
+        int[] pos = {};
+        int x = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == (byte) 0x58) {
+                if (i > 0 && data[i-1] == data[6]) {
+                    x = i + 1;
+                }
+            } else if (x > 0 && data[i] == data[6]) {
+                if (isXdddd(data, x, i)) {
+                    pos = Arrays.copyOf(pos, pos.length + 2);
+                    pos[pos.length-2] = x;
+                    pos[pos.length-1] = i;
+                }
+                x = 0;
+            }
+        }
+        return pos;
+    }
+
+    private static boolean isXdddd(byte[] data, int beginIndex, int endIndex) {
+        int n = endIndex - beginIndex;
+        if ((n & 1) != 0) return false;
+        int i = beginIndex;
+        while (--n > 0) {
+            int hex = parseHex(data[i++]);
+            if (hex < 0
+                    || hex == data[3]   // field separator
+                    || hex == data[4]   // component separator
+                    || hex == data[5]   // repetition separator
+                    || hex == data[6]   // escape character
+                    || hex == data[7]   // subcomponent separator
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int parseHex(int i) {
+        int d = i - 0x30;
+        if (d > 9) {
+            d = i - 0x41;
+            if (d > 6) {
+                d = i - 0x61;
+                if (d > 6) return -1;
+            }
+            if (d >= 0)
+                d += 10;
+        }
+        return d;
     }
 }

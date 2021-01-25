dc0e57771186e1365cd0d99fc0731269bc9959f1
Patrick Peer <ppe@synedra.com>
2021-01-25 13:43:04 +0100

    Added startSequence() und endSequence() methods

--- dcm4che-core/src/main/java/org/dcm4che3/data/Attributes.java
+++ dcm4che-core/src/main/java/org/dcm4che3/data/Attributes.java
@@ -65,6 +65,8 @@ public class Attributes implements Serializable {
     }
 
     public interface SequenceVisitor extends Visitor {
+    	default void startSequence(int sqTag) {};
+    	default void endSequence() {};
         void startItem(int sqTag, int itemIndex);
         void endItem();
     }
@@ -3017,6 +3019,8 @@ public class Attributes implements Serializable {
             if (!visitor.visit(this, tags[i], vrs[i], values[i]))
                 return false;
             if (visitNestedDatasets && (values[i] instanceof Sequence)) {
+                if (visitor instanceof SequenceVisitor)
+                    ((SequenceVisitor) visitor).startSequence(tags[i]);
                 int itemIndex = 0;
                 for (Attributes item : (Sequence) values[i]) {
                     if (visitor instanceof SequenceVisitor)
@@ -3027,6 +3031,8 @@ public class Attributes implements Serializable {
                         ((SequenceVisitor) visitor).endItem();
                     itemIndex++;
                 }
+                if (visitor instanceof SequenceVisitor)
+                    ((SequenceVisitor) visitor).endSequence();
             }
         }
         return true;

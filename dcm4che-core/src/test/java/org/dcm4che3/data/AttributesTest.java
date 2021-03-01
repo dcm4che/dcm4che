c0c34a7c4926ca239bfbb7677beb331ac250ed45
Gunter Zeilinger <gunterze@protonmail.com>
2021-03-01 13:02:17 +0100

    Creator-less Private Tag cause equals() to throw Null Pointer Exception #900

--- dcm4che-core/src/test/java/org/dcm4che3/data/AttributesTest.java
+++ dcm4che-core/src/test/java/org/dcm4che3/data/AttributesTest.java
@@ -180,6 +180,7 @@ public class AttributesTest {
         a1.setString(0x00090010, VR.LO, "CREATOR1");
         a1.setString(0x00091010, VR.LO, "VALUE1");
         Attributes a2 = new Attributes();
+        a2.setString(0x00090020, VR.LO, "CREATOR2");
         a2.setString(0x00091010, VR.LO, "VALUE1");
         assertTrue(!a1.equals(a2));
         assertTrue(!a2.equals(a1));

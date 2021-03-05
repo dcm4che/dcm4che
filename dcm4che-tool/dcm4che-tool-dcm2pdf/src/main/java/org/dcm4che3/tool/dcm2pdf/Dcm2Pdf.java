5ee3251818da9f827f1098ba639e6781b632565a
Vrinda <vrinda.nayak@j4care.com>
2021-03-05 16:39:41 +0100

    dcm2pdf utility : Extract original unpadded encapsulated document of odd length #907

--- dcm4che-tool/dcm4che-tool-dcm2pdf/src/main/java/org/dcm4che3/tool/dcm2pdf/Dcm2Pdf.java
+++ dcm4che-tool/dcm4che-tool-dcm2pdf/src/main/java/org/dcm4che3/tool/dcm2pdf/Dcm2Pdf.java
@@ -65,7 +65,6 @@ public class Dcm2Pdf {
     private static final Logger LOG = LoggerFactory.getLogger(Dcm2Pdf.class);
     private static final ResourceBundle rb = ResourceBundle.getBundle("org.dcm4che3.tool.dcm2pdf.messages");
     private FileType fileType;
-    private boolean encapsulatedDocLength;
 
     public static void main(String[] args) {
         try {
@@ -117,10 +116,6 @@ public class Dcm2Pdf {
                 .hasArg(false)
                 .desc(rb.getString("stl"))
                 .build());
-        opts.addOption(Option.builder()
-                .longOpt("encapsulatedDocLength")
-                .desc(rb.getString("encapsulatedDocLength"))
-                .build());
         opts.addOptionGroup(group);
         return CLIUtils.parseComandLine(args, opts, rb, Dcm2Pdf.class);
     }
@@ -134,7 +129,6 @@ public class Dcm2Pdf {
                         ? FileType.OBJ
                         : cl.hasOption("stl")
                             ? FileType.STL :  FileType.PDF;
-        this.encapsulatedDocLength = cl.hasOption("encapsulatedDocLength");
     }
 
     enum FileType {
@@ -220,14 +214,10 @@ public class Dcm2Pdf {
 
             FileOutputStream fos = new FileOutputStream(dest.toFile());
             byte[] value = (byte[]) attributes.getValue(Tag.EncapsulatedDocument);
-            if (encapsulatedDocLength && attributes.getLong(Tag.EncapsulatedDocumentLength, 0L) % 2 != 0)
-                fos.write(value, 0, value.length - 1);
-            else {
-                if (fileType == FileType.PDF || fileType == FileType.STL)
-                    fos.write(value);
-                else
-                    fos.write(value, 0, value.length - 1);
-            }
+            fos.write(value, 0, value.length - 1);
+            byte lastByte = value[value.length - 1];
+            if (lastByte != 0)
+                fos.write(lastByte);
             System.out.println(MessageFormat.format(rb.getString("converted"), src, dest));
         } catch (Exception e) {
             System.out.println(MessageFormat.format(rb.getString("failed"), src, e.getMessage()));

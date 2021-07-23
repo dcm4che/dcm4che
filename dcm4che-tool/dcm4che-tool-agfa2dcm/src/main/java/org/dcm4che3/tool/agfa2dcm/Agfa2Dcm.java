0ecc87443a7ab4a9f9f28791ba2eec53631d1590
Gunter Zeilinger <gunterze@protonmail.com>
2021-07-23 18:10:34 +0200

    agfa2dcm: fails to extract more than one DICOM file encoded with Implicit VR Little Endian Transfer Syntax fix #992

--- dcm4che-tool/dcm4che-tool-agfa2dcm/src/main/java/org/dcm4che3/tool/agfa2dcm/Agfa2Dcm.java
+++ dcm4che-tool/dcm4che-tool-agfa2dcm/src/main/java/org/dcm4che3/tool/agfa2dcm/Agfa2Dcm.java
@@ -130,9 +130,9 @@ public class Agfa2Dcm {
             if (!Arrays.equals(header, ARCHIVE_BLOB))
                 throw new IOException ("Missing ARCHIVE-BLOB header");
 
-            DicomInputStream dis;
+            DicomInputStream dis = null;
             do {
-                dis = new DicomInputStream(in, 116);
+                dis = new DicomInputStream(in, dis == null || dis.explicitVR() ? 116 : 120);
                 dis.setIncludeBulkData(DicomInputStream.IncludeBulkData.NO);
                 Attributes attrs = dis.readDataset(-1, Agfa2Dcm::isArchiveBlobTag);
                 list.add(new Part(format.format(attrs), dis.getPosition()));

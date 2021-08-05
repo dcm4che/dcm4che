18508e99fd6d623144e2d941d82ad0d44928dbc2
Hermann Czedik-Eysenberg <hermann.czedik-eysenberg@agfa.com>
2021-08-05 09:07:32 +0200

    Make search path for jacoco reports more specific

--- .github/workflows/build-with-sonar.sh
+++ .github/workflows/build-with-sonar.sh
@@ -3,4 +3,4 @@
 set -e
 
 mvn -B -P test-coverage verify
-mvn -B org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.coverage.jacoco.xmlReportPaths=$(find "$(pwd)" -path '*jacoco.xml' | tr '\n' ',')
+mvn -B org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.coverage.jacoco.xmlReportPaths=$(find "$(pwd)" -path '*/target/site/*/jacoco.xml' | tr '\n' ',')

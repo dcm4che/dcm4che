3543c9dd8d3f101ba4f9b6099b02585fdd0f0c5b
Gunter Zeilinger <gunterze@protonmail.com>
2021-08-10 12:24:48 +0200

    Revert [LIB-317] Pack VR lookup table to 1/4 fix #1009

--- dcm4che-core/src/main/java/org/dcm4che3/data/VR.java
+++ dcm4che-core/src/main/java/org/dcm4che3/data/VR.java
@@ -236,20 +236,19 @@ public enum VR {
         this.inlineBinary = inlineBinary;
     }
 
-    private static int indexOf(int code) {
-        int code1 = code - 0x4141;
-        return (code1 & 0xffffe0e0) == 0 ? ((code1 & 0xff00) >> 3) + (code1 & 0xff) : -1;
+    private static int indexOf(VR vr) {
+        return vr.code - AE.code;
     }
 
-    private static final VR[] VALUE_OF = new VR[indexOf(UV.code)+1];
+    private static final VR[] VALUE_OF = new VR[indexOf(UV)+1];
     static {
         for (VR vr : VR.values())
-            VALUE_OF[indexOf(vr.code)] = vr;
+            VALUE_OF[indexOf(vr)] = vr;
     }
 
     public static VR valueOf(int code) {
         try {
-            VR vr = VALUE_OF[indexOf(code)];
+            VR vr = VALUE_OF[code - AE.code];
             if (vr != null)
                 return vr;
         } catch (IndexOutOfBoundsException e) {}

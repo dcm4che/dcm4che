ac29cd100ce5761d93ac2090bd1d143ce1127706
Hermann Czedik-Eysenberg <hermann-agfa@czedik.net>
2021-09-15 17:11:23 +0200

    Do not run sonar analysis for external pull requests
    ... as they do not have a SONAR_TOKEN (for security reasons) and would therefore fail anyways.

--- .github/workflows/build-with-sonar.sh
+++ .github/workflows/build-with-sonar.sh
@@ -3,4 +3,12 @@
 set -e
 
 mvn -B -P test-coverage verify
-mvn -B org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.coverage.jacoco.xmlReportPaths=$(find "$(pwd)" -path '*/target/site/*/jacoco.xml' | tr '\n' ',')
+
+if [ -z "$SONAR_TOKEN" ]
+then
+  echo "Cannot run sonar analysis as there is no SONAR_TOKEN. \
+  This happens for external pull requests (from forks). See https://jira.sonarsource.com/browse/MMF-1371"
+else
+  mvn -B org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
+  -Dsonar.coverage.jacoco.xmlReportPaths=$(find "$(pwd)" -path '*/target/site/*/jacoco.xml' | tr '\n' ',')
+fi

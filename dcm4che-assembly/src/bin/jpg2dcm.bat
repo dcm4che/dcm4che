17a5affd95564b6e3101f8367999c6304ee1ee4e
Gunter Zeilinger <gunterze@protonmail.com>
2021-12-20 16:44:38 +0100

    Utilities: Replace log4j-1.2.17 by logback-classic-1.2.9 fix #1050

--- dcm4che-assembly/src/bin/jpg2dcm.bat
+++ dcm4che-assembly/src/bin/jpg2dcm.bat
@@ -47,8 +47,8 @@ set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-imageio-${project.version}.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-net-${project.version}.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\dcm4che-tool-common-${project.version}.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-api-${slf4j.version}.jar
-set CP=%CP%;%DCM4CHE_HOME%\lib\slf4j-log4j12-${slf4j.version}.jar
-set CP=%CP%;%DCM4CHE_HOME%\lib\log4j-${log4j.version}.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\logback-core-${logback.version}.jar
+set CP=%CP%;%DCM4CHE_HOME%\lib\logback-classic-${logback.version}.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\jakarta.activation-${jakarta.activation.version}.jar
 set CP=%CP%;%DCM4CHE_HOME%\lib\commons-cli-${commons-cli.version}.jar
 

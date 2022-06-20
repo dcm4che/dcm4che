5554dc2b5347f774bd8406198412e98177a30b0f
mdoris <mike.doris@agfa.com>
2022-06-20 15:27:50 -0400

    Decrement performing ops when exception occurs writing response (#1204)
    * IEI-196201 decrement the performing operations counter when the pduencoder throws an exception to prevent stuck threads
    
    (cherry picked from commit 66294c56aa8a7af9577576ae6f1b4f01f75dd226)
    (cherry picked from commit 2653b59bc0d162dcbf8bb469cfad8a76614c122e)
    
    * increase log level when exception while processing dimse rq
    
    (cherry picked from commit 39e34b6d088d33144a9a31ab7433c2eb3467f0b5)
    (cherry picked from commit 53c919fd33b6d3a76c68e852d5da01bbd3041ecf)
    
    * IEI-196201 Update the unit test to work with changes in master
    
    (cherry picked from commit 281e56584a4fd4e607af663e3b7a822ce366e24b)
    
    * IEI-196201 fix code smells in test
    
    (cherry picked from commit 80332e242465a17d554633a41002e46eadba4d49)
    
    * IEI-196201 use shutdownNow()

---
+++ dcm4che-net/src/test/java/org/dcm4che3/net/AssociationTest.java
@@ -0,0 +1,106 @@
+package org.dcm4che3.net;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+import org.dcm4che3.data.Attributes;
+import org.dcm4che3.data.Tag;
+import org.dcm4che3.data.VR;
+import org.dcm4che3.net.pdu.AAssociateAC;
+import org.dcm4che3.net.pdu.AAssociateRJ;
+import org.dcm4che3.net.pdu.PresentationContext;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
+
+/**
+ * Unit tests for the org.dcm4che3.net.Association class
+ */
+public class AssociationTest {
+
+    @Test
+    public void writeDimseRsp_pduEncoderThrowsException_performingRqCounterDecremented() throws IOException {
+        Socket socket = new BadSocket();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        Device device = new Device();
+        device.setExecutor(executorService);
+        device.setAssociationMonitor(new AssociationMonitor() {
+            @Override public void onAssociationEstablished(Association as) {
+            }
+
+            @Override public void onAssociationFailed(Association as, Throwable e) {
+            }
+
+            @Override public void onAssociationRejected(Association as, AAssociateRJ aarj) {
+            }
+
+            @Override public void onAssociationAccepted(Association as) {
+            }
+        });
+
+        Connection connection = new Connection();
+        connection.setDevice(device);
+
+        Association association = new Association(null, connection, socket);
+        association.handle(new AAssociateAC());
+
+        PresentationContext presentationContext = new PresentationContext(1, "as", "ts");
+        Attributes cmd = new Attributes();
+        cmd.setInt(Tag.CommandField, VR.US, 0x8021);
+        cmd.setInt(Tag.Status, VR.US, Status.Success);
+
+        int performingCount = association.getPerformingOperationCount();
+        Assert.assertThrows("OutputStream did not throw exception", BadSocketException.class, new ThrowingRunnable() {
+            @Override public void run() throws Throwable {
+                association.writeDimseRSP(presentationContext, cmd);
+            }
+        });
+
+        Assert.assertEquals("Performing Operation count did not decrement",
+                performingCount - 1, association.getPerformingOperationCount());
+        executorService.shutdownNow();
+    }
+
+    private class BadSocket extends Socket {
+
+        @Override public InputStream getInputStream() {
+            return new DummyInputStream();
+        }
+
+        @Override public OutputStream getOutputStream() {
+            return new BadOutputStream();
+        }
+    }
+
+    private class DummyInputStream extends InputStream {
+
+        Semaphore semaphore = new Semaphore(-1);
+
+        @Override public int read() throws IOException {
+            try {
+                // Simulate the blocking read for nextPDU
+                semaphore.acquire();
+            } catch (InterruptedException e) {
+            }
+            return 0;
+        }
+    }
+
+    private class BadOutputStream extends OutputStream {
+
+        @Override public void write(int b) throws IOException {
+            throw new BadSocketException();
+        }
+    }
+
+    private class BadSocketException extends SocketException {
+    }
+}

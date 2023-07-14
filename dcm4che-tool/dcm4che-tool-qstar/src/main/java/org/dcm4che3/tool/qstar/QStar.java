0c4e410c1b7329c922b235fb65085a3a8a303c0a
Gunter Zeilinger <gunterze@protonmail.com>
2023-07-14 17:00:07 +0200

    QStar SOAP Client to prefetch/retrieve or fetch file information of specified files #1311

--- dcm4che-tool/dcm4che-tool-qstar/src/main/java/org/dcm4che3/tool/qstar/QStar.java
+++ dcm4che-tool/dcm4che-tool-qstar/src/main/java/org/dcm4che3/tool/qstar/QStar.java
@@ -46,9 +46,7 @@ import org.dcm4che3.tool.common.CLIUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.ws.BindingProvider;
 import java.math.BigInteger;
-import java.time.Instant;
 import java.util.*;
 
 /**
@@ -59,50 +57,11 @@ public class QStar {
 
     private static final Logger LOG = LoggerFactory.getLogger(QStar.class);
     private static final ResourceBundle rb = ResourceBundle.getBundle("org.dcm4che3.tool.qstar.messages");
-    private static final WSWebService service = new WSWebService();
-    private static final ObjectFactory factory = new ObjectFactory();
-    private static final String[] ACCESS_STATES = {
-            "0 - ACCESS_STATE_NONE",
-            "1 - ACCESS_STATE_EMPTY",
-            "2 - ACCESS_STATE_UNSTABLE",
-            "3 - ACCESS_STATE_STABLE",
-            "4 - ACCESS_STATE_OUT_OF_CACHE",
-            "5 - ACCESS_STATE_OFFLINE"
-    };
-    private static final String[] DETAILED_ACCESS_STATES = {
-            "0 - DETAILED_ACCESS_STATE_NONE",
-            "1 - DETAILED_ACCESS_STATE_EMPTY",
-            "2 - DETAILED_ACCESS_STATE_CACHED_PRIMARY",
-            "3 - DETAILED_ACCESS_STATE_CACHED_MIG",
-            "4 - DETAILED_ACCESS_STATE_CACHED_MIG_OUT",
-            "5 - DETAILED_ACCESS_STATE_CACHED_MIG_REP",
-            "6 - DETAILED_ACCESS_STATE_CACHED_REP_OUT",
-            "7 - DETAILED_ACCESS_STATE_MIGRATED",
-            "8 - DETAILED_ACCESS_STATE_MIGRATED_OUT",
-            "9 - DETAILED_ACCESS_STATE_ARCHIVED_REP",
-            "10 - DETAILED_ACCESS_STATE_ARCHIVED_OUT",
-            "11 - DETAILED_ACCESS_STATE_REPLICATED_OUT",
-            "12 - DETAILED_ACCESS_STATE_REPLICATED",
-            "13 - DETAILED_ACCESS_STATE_OUT_OF_CACHE",
-            "14 - DETAILED_ACCESS_STATE_OFFLINE"
-    };
-    private static final String[] JOB_STATUS_NAMES = {
-            "0",
-            "1 - INQUEUE",
-            "2 - PROCESSING",
-            "3 - COMPLETED",
-            "4 - FAILED",
-            "5 - PARTIALLY_COMPLETED"
-    };
-
     private final WSWebServiceSoapPort port;
     private WSUserLoginResponse userLogin;
 
     private QStar(String url) {
-        port = service.getWSWebServiceSoapPort();
-        BindingProvider bindingProvider = (BindingProvider) port;
-        Map<String, Object> reqCtx = bindingProvider.getRequestContext();
-        reqCtx.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, url);
+        port = QStarUtils.getWSWebServiceSoapPort(url);
     }
 
     public static void main(String[] args) {
@@ -279,16 +238,7 @@ public class QStar {
 
     private boolean login(String userName, String userPassword) {
         try {
-            WSUserLoginRequest rq = factory.createWSUserLoginRequest();
-            rq.setUserName(userName);
-            rq.setUserPassword(userPassword);
-            LOG.info("<< WSUserLoginRequest{userName='{}', userPassword='{}'}", userName, userPassword);
-            userLogin = port.wsUserLogin(rq);
-            LOG.info(">> WSUserLoginResponse{result={}, resultString='{}', wsdlVersion='{}', userToken='{}'}",
-                    userLogin.getResult(),
-                    userLogin.getResultString(),
-                    userLogin.getWsdlVersion(),
-                    userLogin.getUserToken());
+            userLogin = QStarUtils.login(port, userName, userPassword);
             return true;
         } catch (Exception e) {
             LOG.info("Login Failed: {}", e.getMessage(), e);
@@ -298,11 +248,7 @@ public class QStar {
 
     private void logout() {
         try {
-            WSUserLogoutRequest rq = factory.createWSUserLogoutRequest();
-            rq.setUserToken(userLogin.getUserToken());
-            LOG.info("<< WSUserLogoutRequest{userToken='{}'}", userLogin.getUserToken());
-            WSUserLogoutResponse userLogout = port.wsUserLogout(rq);
-            LOG.info(">> WSUserLogoutResponse{result={}}", userLogout.getResult());
+            QStarUtils.logout(port, userLogin);
         } catch (Exception e) {
             LOG.info("Logout Failed: {}", e.getMessage(), e);
         }
@@ -310,13 +256,7 @@ public class QStar {
 
     private WSGetFileInfoResponse getFileInfo(String filePath) {
         try {
-            WSGetFileInfoRequest rq = factory.createWSGetFileInfoRequest();
-            rq.setSFileFullPath(filePath);
-            rq.setUserToken(userLogin.getUserToken());
-            LOG.info("<< WSGetFileInfoRequest{sFileFullPath='{}', userToken='{}'}", filePath, userLogin.getUserToken());
-            WSGetFileInfoResponse fileInfo = port.wsGetFileInfo(rq);
-            LOG.info(">> WSGetFileInfoResponse{status={}, info={}}", fileInfo.getStatus(), toString(fileInfo.getInfo()));
-            return fileInfo;
+            return QStarUtils.getFileInfo(port, userLogin, filePath);
         } catch (Exception e) {
             LOG.info("GetFileInfo Failed: {}", e.getMessage(), e);
         }
@@ -325,42 +265,16 @@ public class QStar {
 
     private BigInteger batchFileRetrieve(long jobPriority, List<String> fileList, String targetDir) {
         try {
-            WSBatchFileRetrieveRequest rq = factory.createWSBatchFileRetrieveRequest();
-            rq.setJobPriority(jobPriority);
-            rq.setFileCount(BigInteger.valueOf(fileList.size()));
-            rq.setFileList(createWSFileList(fileList));
-            rq.setTargetDir(targetDir);
-            rq.setUserToken(userLogin.getUserToken());
-            LOG.info("<< WSBatchFileRetrieveRequest{jobPriority={}, fileCount='{}', targetDir='{}', userToken='{}'}",
-                    jobPriority,
-                    fileList.size(),
-                    targetDir,
-                    userLogin.getUserToken());
-            WSBatchFileRetrieveResponse fileRetrieve = port.wsBatchFileRetrieve(rq);
-            LOG.info(">> WSBatchFileRetrieveResponse{jobId={}}", fileRetrieve.getJobId());
-            return fileRetrieve.getJobId();
+            return QStarUtils.batchFileRetrieve(port, userLogin, jobPriority, fileList, targetDir).getJobId();
         } catch (Exception e) {
             LOG.info("BatchFileRetrieve Failed: {}", e.getMessage(), e);
         }
         return null;
     }
 
-    private static WSFileList createWSFileList(List<String> fileList) {
-        WSFileList wsFileList = factory.createWSFileList();
-        wsFileList.getFileName().addAll(fileList);
-        return wsFileList;
-    }
-
     private int batchJobStatus(BigInteger jobId) {
         try {
-            WSBatchJobStatusRequest rq = factory.createWSBatchJobStatusRequest();
-            rq.setJobId(jobId);
-            rq.setUserToken(userLogin.getUserToken());
-            LOG.info("<< WSBatchJobStatusRequest{jobId={}, userToken='{}'}", jobId, userLogin.getUserToken());
-            WSBatchJobStatusResponse jobStatus = port.wsBatchJobStatus(rq);
-            LOG.info(">> WSBatchJobStatusResponse{jobStatus={}}",
-                    toString(jobStatus.getJobStatus(), JOB_STATUS_NAMES));
-            return (int) jobStatus.getJobStatus();
+            return (int) QStarUtils.batchJobStatus(port, userLogin, jobId).getJobStatus();
         } catch (Exception e) {
             LOG.info("BatchJobStatus Failed: {}", e.getMessage(), e);
         }
@@ -369,16 +283,7 @@ public class QStar {
 
     private int batchJobObjectStatus(BigInteger jobId, String file) {
         try {
-            WSBatchJobObjectStatusRequest rq = factory.createWSBatchJobObjectStatusRequest();
-            rq.setJobId(jobId);
-            rq.setJobObjectName(file);
-            rq.setUserToken(userLogin.getUserToken());
-            LOG.info("<< WSBatchJobObjectStatusRequest{jobId={}, jobObjectName='{}', userToken='{}'}",
-                    jobId, file, userLogin.getUserToken());
-            WSBatchJobObjectStatusResponse jobObjectStatus = port.wsBatchJobObjectStatus(rq);
-            LOG.info(">> WSBatchJobObjectStatusResponse{jobObjectStatus={}}",
-                    toString(jobObjectStatus.getJobObjectStatus(), JOB_STATUS_NAMES));
-            return (int) jobObjectStatus.getJobObjectStatus();
+            return (int) QStarUtils.batchJobObjectStatus(port, userLogin, jobId, file).getJobObjectStatus();
         } catch (Exception e) {
             LOG.info("BatchJobObjectStatus Failed: {}", e.getMessage(), e);
         }
@@ -387,92 +292,10 @@ public class QStar {
 
     private void purgeFile(String filePath) {
         try {
-            WSMMPurgeFileRequest rq = factory.createWSMMPurgeFileRequest();
-            rq.setFileName(filePath);
-            rq.setUserToken(userLogin.getUserToken());
-            LOG.info("<< WSMMPurgeFileRequest{fileName='{}', userToken='{}'}", filePath, userLogin.getUserToken());
-            WSMMPurgeFileResponse purgeFile = port.wsmmPurgeFile(rq);
-            LOG.info(">> WSMMPurgeFileResponse{error={}, description={}}", purgeFile.getError(), purgeFile.getDescription());
+            QStarUtils.purgeFile(port, userLogin, filePath);
         } catch (Exception e) {
             LOG.info("GetFileInfo Failed: {}", e.getMessage(), e);
         }
     }
 
-    private static String toString(long i, String[] values) {
-        try {
-            return values[(int) i];
-        } catch (IndexOutOfBoundsException e) {
-            return Long.toString(i);
-        }
-    }
-
-    private static String toString(WSFileInfo info) {
-        if (info == null) return "";
-        StringBuilder sb = new StringBuilder(1024);
-        sb.append("\n  WSFileInfo{\n    size=").append(info.getSize());
-        sb.append(",\n    mode=").append(info.getMode());
-        sb.append(",\n    uid=").append(info.getUid());
-        sb.append(",\n    gid=").append(info.getGid());
-        sb.append(",\n    nlink=").append(info.getNlink());
-        sb.append(",\n    flags=").append(info.getFlags());
-        sb.append(",\n    presentCount=").append(info.getPresentCount());
-        sb.append(",\n    primaryCount=").append(info.getPrimaryCount());
-        sb.append(",\n    replicatedCount=").append(info.getReplicatedCount());
-        sb.append(",\n    archivedCount=").append(info.getArchivedCount());
-        sb.append(",\n    pageSize=").append(info.getPageSize());
-        sb.append(",\n    aTime=").append(toInstant(info.getATime()));
-        sb.append(",\n    mTime=").append(toInstant(info.getMTime()));
-        sb.append(",\n    cTime=").append(toInstant(info.getCTime()));
-        sb.append(",\n    crTime=").append(toInstant(info.getCrTime()));
-        sb.append(",\n    cmTime=").append(toInstant(info.getCmTime()));
-        sb.append(",\n    onOff=").append(info.getOnOff());
-        sb.append(",\n    retentionEndTime=").append(toInstant(info.getRetentionEndTime()));
-        sb.append(",\n    digest=").append(quote(info.getDigest()));
-        sb.append(",\n    objectId=").append(quote(info.getObjectId()));
-        sb.append(",\n    objectIdLength=").append(info.getObjectIdLength());
-        sb.append(",\n    digestType=").append(info.getDigestType());
-        sb.append(",\n    encryptionType=").append(info.getEncryptionType());
-        sb.append(",\n    cryptoFlags=").append(info.getCryptoFlags());
-        sb.append(",\n    flocExtents=[");
-        for (WSFileExtentInfo extent : info.getFlocExtents().getExtent()) {
-            sb.append("\n      WSFileExtentInfo{\n        offset=").append(extent.getOffset());
-            sb.append(",\n        size=").append(extent.getSize());
-            sb.append(",\n        vol=").append(extent.getVol());
-            sb.append(",\n        pos=").append(extent.getPos());
-            sb.append(",\n        isOffline=").append(extent.getIsOffline());
-            sb.append(",\n        mediaBarcode=").append(quote(extent.getMediaBarcode()));
-            sb.append(",\n        libraryName=").append(quote(extent.getLibraryName()));
-            sb.append(",\n        offlineLocation=").append(quote(extent.getOfflineLocation()));
-            sb.append(",\n        extentCopies=[");
-            for (WSFileExtentCopyInfo copy : extent.getExtentCopies().getCopies()) {
-                sb.append("\n          WSFileExtentCopyInfo{\n            isOffline=").append(copy.getIsOffline());
-                sb.append(",\n            mediaBarcode=").append(quote(copy.getMediaBarcode()));
-                sb.append(",\n            offlineLocation=").append(quote(copy.getOfflineLocation()));
-                sb.append(",\n            copyNumber=").append(copy.getCopyNumber());
-                sb.append(",\n            deviceName=").append(quote(copy.getDeviceName()));
-                sb.append(",\n            slotNumber=").append(copy.getSlotNumber());
-                sb.append(",\n            side=").append(quote(copy.getSide()));
-                sb.append(",\n            copyInSet=").append(copy.getCopyInSet());
-                sb.append(",\n            copyInDB=").append(copy.getCopyInDB());
-                sb.append(",\n            copyInDevice=").append(copy.getCopyInDevice());
-                sb.append('}');
-            }
-            sb.append("],\n        copiesCount=").append(extent.getCopiesCount());
-            sb.append('}');
-        }
-        sb.append("],\n    extCount=").append(info.getExtCount());
-        sb.append(",\n    flocError=").append(quote(info.getFlocError()));
-        sb.append(",\n    stateAccess=").append(toString(info.getStateAccess(), ACCESS_STATES));
-        sb.append(",\n    stateAccessDetailed=").append(toString(info.getStateAccessDetailed(), DETAILED_ACCESS_STATES));
-        sb.append('}');
-        return sb.toString();
-    }
-
-    private static Object toInstant(BigInteger value) {
-        return value.signum() == 0 ? "" : Instant.ofEpochSecond(value.longValue());
-    }
-
-    private static String quote(String value) {
-        return value == null ? "" : '\'' + value + '\'';
-    }
 }

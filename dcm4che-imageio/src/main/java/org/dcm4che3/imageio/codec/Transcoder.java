28e7c05b24d226044f89839aac2cf02e85aa5390
Benjamin Ganley <william00179@users.noreply.github.com>
2024-04-02 23:32:32 +1300

    Update Transcoder.java
    If reading a compressed frame the padding will always be negative and do nothing in the skipFully function because of the negative padding check. 
    
    This is unless the compressed size is actually larger than the uncompressed size (due to bad compression algo selection) and then this will result in a positive number and cause an EOF Exception.
    
    We should only skip if the  frame hasn't been decompressed.

--- dcm4che-imageio/src/main/java/org/dcm4che3/imageio/codec/Transcoder.java
+++ dcm4che-imageio/src/main/java/org/dcm4che3/imageio/codec/Transcoder.java
@@ -593,7 +593,8 @@ public class Transcoder implements Closeable {
                     : originalBi;
             compressFrame(i);
         }
-        dis.skipFully(padding);
+        if(decompressor == null)
+            dis.skipFully(padding);
         dos.writeHeader(Tag.SequenceDelimitationItem, null, 0);
     }
 

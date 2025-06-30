18bfc27a467c44abd89b099ba994a07eccb5ba7a
Balder Van Camp <balder.vancamp@agfa.com>
2025-06-30 11:10:32 +0200

    IEI-368053 more unused imports

--- dcm4che-net/src/main/java/org/dcm4che3/net/ApplicationEntity.java
+++ dcm4che-net/src/main/java/org/dcm4che3/net/ApplicationEntity.java
@@ -46,7 +46,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.EnumSet;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
